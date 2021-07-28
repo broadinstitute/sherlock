1bd421af7c130eb073a5e91a6378ef80c11422cd
mflinn-broad <mflinn@broadinstitute.org>
2021-07-28 17:23:46 -0400

    a bunch of stuff for setup to work with a database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/jmoiron/sqlx"
+)
 
 func main() {
-	fmt.Println("This is where the server goes")
+	db, err := sqlx.Connect("pgx", os.Getenv("POSTGRESQL_URL"))
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	log.Println("Successfully connected to database")
+
+	log.Println("Executing database migration")
+	m, err := migrate.New(
+		"file://db/migrations",
+		os.Getenv("POSTGRESQL_URL"),
+	)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if err := m.Up(); err != nil {
+		log.Println(err)
+	}
+
+	log.Println("database migration complete")
+
+	log.Println("starting sherlock server")
+	http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello sherlock!")
 }
--- pkg/config/config.go
+++
@@ -1,3 +0,0 @@
-package config
-
-// Some general config parsing stuff that is public and able to be imported in other projects with `go get`
