837c20fd2307011690819b458eaee09a94dcf807
mflinn-broad <mflinn@broadinstitute.org>
2021-08-05 16:51:11 -0400

    server is now working with gin and gorm but unit tests are still broken

--- internal/services/queries.go
+++
@@ -1,7 +0,0 @@
-package services
-
-// SQL query strings for interacting with services tracked by Sherlock
-const (
-	// selectAll is a query string to retrive all services from the db
-	selectAll string = "SELECT * FROM services;"
-)
