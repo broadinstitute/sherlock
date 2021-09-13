561b4bb59dbb7b7ab2ec371cd25b85a8d0f9d824
mflinn-broad <mflinn@broadinstitute.org>
2021-09-13 15:04:04 -0400

    now properly returns all the build metadata including service info in POST /builds response

--- internal/builds/models.go
+++ internal/builds/models.go
@@ -46,5 +46,12 @@ func (db dataStore) listAll() ([]Build, error) {
 
 func (db dataStore) createNew(newBuild *Build) (*Build, error) {
 	err := db.Create(newBuild).Error
+	if err != nil {
+		return nil, fmt.Errorf("error persisting new build: %v", err)
+	}
+	// retrieve the same build record back from DB so it can be returned with associations updated
+	// gorm will not update the associations in the input struct when performing create operations,
+	// even though those associations will be modeled properly in the db
+	err = db.Preload("Service").First(newBuild, newBuild.ID).Error
 	return newBuild, err
 }
