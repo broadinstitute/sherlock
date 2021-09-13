84ad286a24c98aa38dee016e903ddc12d8cd9e15
mflinn-broad <mflinn@broadinstitute.org>
2021-09-13 12:43:41 -0400

    tests for listing builds

--- internal/builds/handlers_test.go
+++ internal/builds/handlers_test.go
@@ -1,11 +1,16 @@
 package builds
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/broadinstitute/sherlock/internal/services"
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -22,6 +27,86 @@ func TestListBuilds(t *testing.T) {
 			expectedBuilds: []Build{},
 			expectedCode:   http.StatusOK,
 			expectedError:  nil,
+		},
+		{
+			name: "one existing build",
+			expectedBuilds: []Build{
+				{
+					VersionString: "imagerepo.io/test:0.1.0",
+					CommitSha:     "f2f2f23",
+					BuildURL:      "https://build.job.out/blah",
+					BuiltAt:       time.Now(),
+					ServiceID:     1,
+					Service: services.Service{
+						ID:      1,
+						Name:    "tester",
+						RepoURL: "https://tester.repo",
+					},
+				},
+			},
+			expectedError: nil,
+			expectedCode:  http.StatusOK,
+		},
+		{
+			name: "multiple builds and services",
+			expectedBuilds: []Build{
+				{
+					VersionString: "imagerepo.io/test:0.1.0",
+					CommitSha:     "f2f2f23",
+					BuildURL:      "https://build.job.out/blah",
+					BuiltAt:       time.Now(),
+					ServiceID:     1,
+					Service: services.Service{
+						ID:      1,
+						Name:    "tester",
+						RepoURL: "https://tester.repo",
+					},
+				},
+				{
+					VersionString: "imagerepo.io/test:0.2.0",
+					CommitSha:     "l2k3j4",
+					BuildURL:      "https://build.job.out/blah",
+					BuiltAt:       time.Now(),
+					ServiceID:     1,
+					Service: services.Service{
+						ID:      1,
+						Name:    "tester",
+						RepoURL: "https://tester.repo",
+					},
+				},
+				{
+					VersionString: "imagerepo.io/dummyService:0.1.0",
+					CommitSha:     "f2f2f23",
+					BuildURL:      "https://build.job.out/234",
+					BuiltAt:       time.Now(),
+					ServiceID:     2,
+					Service: services.Service{
+						ID:      2,
+						Name:    "dummyServcie",
+						RepoURL: "https://dummy.repo",
+					},
+				},
+				{
+					VersionString: "imagerepo.io/cromwell:1.45.0",
+					CommitSha:     "k2j3h43k",
+					BuildURL:      "https://build.job.out/2345",
+					BuiltAt:       time.Now(),
+					ServiceID:     3,
+					Service: services.Service{
+						ID:      3,
+						Name:    "cromwell",
+						RepoURL: "https://cromwell.repo",
+					},
+				},
+			},
+			expectedError: nil,
+			expectedCode:  http.StatusOK,
+		},
+		{
+			name:           "internal error",
+			expectedBuilds: []Build{},
+			expectedError:  errors.New("some internal error"),
+			expectedCode:   http.StatusInternalServerError,
 		},
 	}
 
@@ -41,6 +126,22 @@ func TestListBuilds(t *testing.T) {
 
 			mockStore.AssertCalled(t, "listAll")
 			assert.Equal(t, testCase.expectedCode, response.Code)
+
+			var gotResponse Response
+			if err := json.NewDecoder(response.Body).Decode(&gotResponse); err != nil {
+				t.Fatalf("error decoding response body: %v\n", err)
+			}
+
+			var expectedResponse Response
+			if testCase.expectedError != nil {
+				expectedResponse = Response{Error: testCase.expectedError.Error()}
+			} else {
+				expectedResponse = Response{Builds: testCase.expectedBuilds}
+			}
+
+			if diff := cmp.Diff(gotResponse, expectedResponse); diff != "" {
+				t.Errorf("unexpected difference in response body: \n%v\n", diff)
+			}
 		})
 	}
 }
