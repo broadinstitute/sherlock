7e6e758dbcc07690c9d494efa6968653ad2a7139
mflinn-broad <mflinn@broadinstitute.org>
2021-09-17 10:30:37 -0400

    working on table driven tests for cli commands

--- internal/cli/createBuild_test.go
+++ internal/cli/createBuild_test.go
@@ -32,23 +32,33 @@ func Test_createBuildCommand(t *testing.T) {
 		},
 	}
 
-	// set up a mock server of the sherlock api
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(&builds.Response{
-			Builds: []builds.BuildResponse{
-				{},
-			},
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// set up a mock server of the sherlock api
+			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				json.NewEncoder(w).Encode(&builds.Response{
+					Builds: []builds.BuildResponse{
+						{},
+					},
+				})
+			}))
+
+			sherlockServerURL = testServer.URL
+			output, err := executeCommand(rootCmd, "builds", "create", testCase.cliArgs[2], "--version-string", testCase.cliArgs[4], "--commit-sha", testCase.cliArgs[6])
+			outputBytes := bytes.NewBufferString(output)
+
+			if testCase.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			// parse the output back into a builds.Response so that we can examine it
+			var cliResponse builds.Response
+			if err := json.NewDecoder(outputBytes).Decode(&cliResponse); err != nil {
+				t.Errorf("error decoding cli output: %v", err)
+			}
 		})
-	}))
-
-	sherlockServerURL = testServer.URL
-	output, err := executeCommand(rootCmd, "builds", "create", "testService", "--version-string", "blah", "--commit-sha", "alsoblah")
-	outputBytes := bytes.NewBufferString(output)
-	assert.NoError(t, err)
-
-	// parse the output back into a builds.Response so that we can examine it
-	var cliResponse builds.Response
-	if err := json.NewDecoder(outputBytes).Decode(&cliResponse); err != nil {
-		t.Errorf("error decoding cli output: %v", err)
 	}
+
 }
