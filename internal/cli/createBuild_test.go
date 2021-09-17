9911e942aba1cf9492892059bfcaecbf0ac99e74
mflinn-broad <mflinn@broadinstitute.org>
2021-09-17 13:15:04 -0400

    test handling for server side errros

--- internal/cli/createBuild_test.go
+++ internal/cli/createBuild_test.go
@@ -3,10 +3,10 @@ package cli
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/broadinstitute/sherlock/internal/builds"
 	"github.com/broadinstitute/sherlock/internal/services"
@@ -15,9 +15,10 @@ import (
 
 func Test_createBuildCommand(t *testing.T) {
 	testCases := []struct {
-		name        string
-		cliArgs     []string
-		expectError bool
+		name               string
+		cliArgs            []string
+		mockServerResponse http.HandlerFunc
+		expectError        error
 	}{
 		{
 			name: "successful create",
@@ -30,49 +31,65 @@ func Test_createBuildCommand(t *testing.T) {
 				"--commit-sha",
 				"l2kj34",
 			},
-			expectError: false,
+			mockServerResponse: func(w http.ResponseWriter, r *http.Request) {
+				json.NewEncoder(w).Encode(&builds.Response{
+					Builds: []builds.BuildResponse{
+						{
+							ID:            1,
+							VersionString: "gcr.io./broad/test-service:1.0.0",
+							CommitSha:     "l2kj34",
+							Service: services.ServiceResponse{
+								ID:   1,
+								Name: "test-service",
+							},
+						},
+					},
+				})
+			},
+			expectError: nil,
+		},
+		{
+			name: "error from server",
+			cliArgs: []string{
+				"builds",
+				"create",
+				"test-service",
+				"--version-string",
+				"gcr.io./broad/test-service:1.0.0",
+				"--commit-sha",
+				"l2kj34",
+			},
+			mockServerResponse: func(w http.ResponseWriter, r *http.Request) {
+				json.NewEncoder(w).Encode(&builds.Response{
+					Error: "some error from sherlock server",
+				})
+			},
+			expectError: errors.New("some error from sherlock server"),
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
 			// set up a mock server of the sherlock api
-			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				json.NewEncoder(w).Encode(&builds.Response{
-					Builds: []builds.BuildResponse{
-						{
-							ID:            1,
-							VersionString: testCase.cliArgs[4],
-							CommitSha:     testCase.cliArgs[6],
-							BuiltAt:       time.Now(),
-							Service: services.ServiceResponse{
-								ID:   1,
-								Name: testCase.cliArgs[2],
-							},
-						},
-					},
-				})
-			}))
+			testServer := httptest.NewServer(testCase.mockServerResponse)
 
 			sherlockServerURL = testServer.URL
-			output, err := executeCommand(rootCmd, "builds", "create", testCase.cliArgs[2], "--version-string", testCase.cliArgs[4], "--commit-sha", testCase.cliArgs[6])
+			output, _ := executeCommand(rootCmd, "builds", "create", testCase.cliArgs[2], "--version-string", testCase.cliArgs[4], "--commit-sha", testCase.cliArgs[6])
 			outputBytes := bytes.NewBufferString(output)
 
-			if testCase.expectError {
-				assert.Error(t, err)
+			if testCase.expectError == nil {
+				// parse the output back into a builds.Response so that we can examine it
+				var cliResponse builds.Response
+				if err := json.NewDecoder(outputBytes).Decode(&cliResponse); err != nil {
+					t.Errorf("error decoding cli output: %v", err)
+				}
+
+				assert.Equal(t, cliResponse.Builds[0].VersionString, testCase.cliArgs[4])
+				assert.Equal(t, cliResponse.Builds[0].CommitSha, testCase.cliArgs[6])
+				assert.Equal(t, cliResponse.Builds[0].Service.Name, testCase.cliArgs[2])
 			} else {
-				assert.NoError(t, err)
+				assert.Contains(t, output, testCase.expectError.Error())
 			}
-
-			// parse the output back into a builds.Response so that we can examine it
-			var cliResponse builds.Response
-			if err := json.NewDecoder(outputBytes).Decode(&cliResponse); err != nil {
-				t.Errorf("error decoding cli output: %v", err)
-			}
-
-			assert.Equal(t, cliResponse.Builds[0].VersionString, testCase.cliArgs[4])
-			assert.Equal(t, cliResponse.Builds[0].CommitSha, testCase.cliArgs[6])
-			assert.Equal(t, cliResponse.Builds[0].Service.Name, testCase.cliArgs[2])
 		})
 	}
 
