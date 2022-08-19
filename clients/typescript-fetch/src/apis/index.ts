30974f4a61bb42d62d92fd226c64aba139c45aa2
Jack Warren <jack@jackwarren.info>
2022-08-19 17:43:55 -0400

    [DDO-2293] Losing my patience (#53)

---
+++ clients/typescript-fetch/src/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppVersionsApi';
+export * from './ChartDeployRecordsApi';
+export * from './ChartReleasesApi';
+export * from './ChartVersionsApi';
+export * from './ChartsApi';
+export * from './ClustersApi';
+export * from './EnvironmentsApi';
+export * from './MiscApi';
