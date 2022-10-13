fa4f9846896b871716fe4fdb83478992ccb71e5a
mflinn-broad <mflinn@broadinstitute.org>
2022-10-13 16:54:12 -0400

    support new defaults for existing records

--- db/migrations/000020_add_helmfile_and_fc_develop_ref_fields.up.sql
+++ db/migrations/000020_add_helmfile_and_fc_develop_ref_fields.up.sql
@@ -8,10 +8,21 @@ alter table v2_changesets
     add if not exists to_firecloud_develop_ref text;
 
 alter table v2_clusters
-    add if not exists helmfile_ref text NOT NULL;
-
-alter table v2_environments
-    add if not exists helmfile_ref text NOT NULL;
+    add if not exists helmfile_ref text DEFAULT 'HEAD' NOT NULL;
 
 alter table v2_clusters
-    add if not exists location text NOT NULL;
+    alter column helmfile_ref DROP DEFAULT;
+
+alter table v2_clusters
+    add if not exists location text DEFAULT 'us-central1-a' NOT NULL;
+
+alter table v2_clusters
+    alter column location DROP DEFAULT;
+    
+alter table v2_environments
+    add if not exists helmfile_ref text DEFAUlT 'HEAD' NOT NULL;
+
+alter table v2_environments
+   alter column helmfile_ref DROP DEFAULT;
+
+
