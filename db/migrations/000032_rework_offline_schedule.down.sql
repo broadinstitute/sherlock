536b6dd0ac7f6950ba36207acd709905e006c7bd
Jack Warren <jack@jackwarren.info>
2023-02-17 16:37:41 -0500

    typo

--- db/migrations/000032_rework_offline_schedule.down.sql
+++ db/migrations/000032_rework_offline_schedule.down.sql
@@ -2,7 +2,7 @@ alter table v2_environments
     rename column offline_schedule_begin_enabled to offline_schedule_enabled;
 
 alter table v2_environments
-    rename column offline_schedule_begin_time to offline_schule_begin;
+    rename column offline_schedule_begin_time to offline_schedule_begin;
 
 alter table v2_environments
     drop column if exists offline_schedule_end_enabled;
--- db/migrations/000032_rework_offline_schedule.up.sql
+++ db/migrations/000032_rework_offline_schedule.up.sql
@@ -2,7 +2,7 @@ alter table v2_environments
     rename column offline_schedule_enabled to offline_schedule_begin_enabled;
 
 alter table v2_environments
-    rename column offline_schule_begin to offline_schedule_begin_time;
+    rename column offline_schedule_begin to offline_schedule_begin_time;
 
 alter table v2_environments
     add if not exists offline_schedule_end_enabled boolean;
