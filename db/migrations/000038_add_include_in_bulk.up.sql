df9d3965a1193e0dbc29a2acd96730a3c21afc94
Jack Warren <jack@jackwarren.info>
2023-03-17 14:03:20 -0400

    migrate the database too

--- db/migrations/000038_add_include_in_bulk.up.sql
+++ db/migrations/000038_add_include_in_bulk.up.sql
@@ -1,2 +1,5 @@
 alter table v2_chart_releases
-    add if not exists include_in_bulk_changesets bool;
+    add if not exists include_in_bulk_changesets bool DEFAULT true;
+
+alter table v2_chart_releases
+    alter column include_in_bulk_changesets DROP DEFAULT;
