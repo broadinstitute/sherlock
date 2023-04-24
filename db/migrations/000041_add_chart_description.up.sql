04375db9236e05fd9bedf9c74c1c920578bb2610
Jack Warren <jack@jackwarren.info>
2023-04-24 17:04:12 -0400

    Revert "add description and playbook url fields"
    This reverts commit 17f1d80d05d56851ab3a21eb8feeab8fa91c99a2.

--- db/migrations/000041_add_chart_description.up.sql
+++
@@ -1,6 +0,0 @@
-alter table v2_charts
-    add if not exists description text;
-
-alter table v2_charts
-    add if not exists playbook_url text;
-
--- internal/controllers/v2controllers/chart.go
+++ internal/controllers/v2controllers/chart.go
@@ -25,8 +25,6 @@ type EditableChart struct {
 	DefaultSubdomain      *string `json:"defaultSubdomain" form:"defaultSubdomain"`                         // When creating, will default to the name of the chart
 	DefaultProtocol       *string `json:"defaultProtocol" form:"defaultProtocol" default:"https"`
 	DefaultPort           *uint   `json:"defaultPort" form:"defaultPort" default:"443"`
-	Description           *string `json:"description" form:"description"`
-	PlaybookURL           *string `json:"playbookURL" form:"playbookURL"`
 }
 
 //nolint:unused
@@ -46,8 +44,6 @@ func (c Chart) toModel(_ *v2models.StoreSet) (v2models.Chart, error) {
 		DefaultSubdomain:      c.DefaultSubdomain,
 		DefaultProtocol:       c.DefaultProtocol,
 		DefaultPort:           c.DefaultPort,
-		Description:           c.Description,
-		PlaybookURL:           c.PlaybookURL,
 	}, nil
 }
 
@@ -94,8 +90,6 @@ func modelChartToChart(model *v2models.Chart) *Chart {
 				DefaultSubdomain:      model.DefaultSubdomain,
 				DefaultProtocol:       model.DefaultProtocol,
 				DefaultPort:           model.DefaultPort,
-				Description:           model.Description,
-				PlaybookURL:           model.PlaybookURL,
 			},
 		},
 	}
