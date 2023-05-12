983c3d8d016dc9f16dda191d91d985887b641b7d
Jack Warren <jack@jackwarren.info>
2023-05-12 16:58:10 -0400

    [DDO-2847] Make Sherlock panic if `cloudsqladmin` role is in the test database (#150)
    * panic if cloudsqladmin exists
    
    * make linter happy

--- internal/db/test_helpers.go
+++ internal/db/test_helpers.go
@@ -1,12 +1,27 @@
 package db
 
 import (
+	"database/sql"
 	"github.com/broadinstitute/sherlock/internal/config"
 	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 	"strings"
 	"testing"
 )
+
+// panicIfLooksLikeCloudSQL does what it says on the tin -- it exits fast and hard if the database has a 'cloudsqladmin'
+// role in it. That's not something Sherlock's migration would ever add but it's there by default on Cloud SQL, so
+// it's an extra gate to make sure we don't accidentally run tests against a remote database.
+func panicIfLooksLikeCloudSQL(t *testing.T, db *gorm.DB) {
+	var cloudSqlAdminRoleExists bool
+	err := db.Raw("SELECT 1 FROM pg_roles WHERE rolname='cloudsqladmin'").Row().Scan(&cloudSqlAdminRoleExists)
+	if err != nil && err != sql.ErrNoRows {
+		t.Fatalf("failed to double-check that the database wasn't running in Cloud SQL: %v", err)
+	}
+	if cloudSqlAdminRoleExists {
+		t.Fatal("this database looks like it is running in Cloud SQL, refusing to proceed with test harness")
+	}
+}
 
 // ConnectAndConfigureFromTest is like Connect and Configure but accepts a testing.T in exchange for never returning an
 // error--the test will be failed instead if there is one.
@@ -20,6 +35,7 @@ func ConnectAndConfigureFromTest(t *testing.T) *gorm.DB {
 	if err != nil {
 		t.Errorf("failed to configure database during test: %v", err)
 	}
+	panicIfLooksLikeCloudSQL(t, gormDB)
 	return gormDB
 }
 
@@ -39,6 +55,7 @@ func Truncate(t *testing.T, db *gorm.DB) {
 		t.Errorf("refusing to truncate database, mode is '%s' instead of 'debug'", mode)
 		return
 	}
+	panicIfLooksLikeCloudSQL(t, db)
 	var statements []string
 	dryRunDB := db.Session(&gorm.Session{
 		// Performance boost, don't transact each delete individually
