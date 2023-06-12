b21bd1f93c2aa2d4a6cf2dc6f1a5ce7dbd91f646
Jack Warren <jack@jackwarren.info>
2023-06-12 15:13:43 -0400

    [DDO-2904] Precalculated CiRun metrics (#187)
    * precalculated metrics
    
    * shorter sql

--- sherlock/internal/models/v2models/metrics_updater.go
+++ sherlock/internal/models/v2models/metrics_updater.go
@@ -197,6 +197,8 @@ func reportDataTypeCounts(ctx context.Context, db *gorm.DB) error {
 		"chart_version": ChartVersion{},
 		"changeset":     Changeset{},
 		"chart_release": ChartRelease{},
+		"ci_identifier": CiIdentifier{},
+		"ci_run":        CiRun{},
 	} {
 		var count int64
 		if err := db.
@@ -242,6 +244,106 @@ func reportEnvironmentStateCounts(ctx context.Context, db *gorm.DB) error {
 					return err
 				}
 				stats.Record(ctx, v2metrics.EnvironmentStateCountMeasure.M(count))
+			}
+		}
+	}
+	return nil
+}
+
+func reportGitHubActionMetrics(ctx context.Context, db *gorm.DB) error {
+	type CompletionCountResult struct {
+		GithubActionsOwner, GithubActionsRepo, GithubActionsWorkflowPath, Status string
+		Count                                                                    int64
+	}
+	for interval, measure := range map[string]*stats.Int64Measure{
+		"1 hour": v2metrics.GithubActions1HourCompletionCountMeasure,
+		"7 days": v2metrics.GithubActions7DayCompletionCountMeasure,
+	} {
+		for retryLabel, attemptCountQuery := range map[string]string{
+			"false": "= 1",
+			"true":  "> 1",
+		} {
+			var results []CompletionCountResult
+			if err := db.Raw(fmt.Sprintf(`
+select v2_ci_runs.github_actions_owner,
+       v2_ci_runs.github_actions_repo,
+       v2_ci_runs.github_actions_workflow_path,
+       v2_ci_runs.status,
+       count(*)
+from v2_ci_runs
+where v2_ci_runs.platform = 'github-actions'
+  and v2_ci_runs.terminal_at >= current_timestamp - '%s'::interval
+  and v2_ci_runs.started_at is not null
+  and v2_ci_runs.github_actions_attempt_number %s
+group by v2_ci_runs.github_actions_owner, 
+         v2_ci_runs.github_actions_repo, 
+         v2_ci_runs.github_actions_workflow_path, 
+         v2_ci_runs.status
+`, interval, attemptCountQuery)).Scan(&results).Error; err != nil {
+				return err
+			}
+			ctx, err := tag.New(ctx,
+				tag.Upsert(v2metrics.GithubActionsRetryKey, retryLabel))
+			if err != nil {
+				return err
+			}
+			for _, result := range results {
+				ctx, err := tag.New(ctx,
+					tag.Upsert(v2metrics.GithubActionsRepoKey, fmt.Sprintf("%s/%s", result.GithubActionsOwner, result.GithubActionsRepo)),
+					tag.Upsert(v2metrics.GithubActionsWorkflowFileKey, result.GithubActionsWorkflowPath),
+					tag.Upsert(v2metrics.GithubActionsOutcomeKey, result.Status))
+				if err != nil {
+					return err
+				}
+				stats.Record(ctx, measure.M(result.Count))
+			}
+		}
+	}
+	type TotalDurationResult struct {
+		GithubActionsOwner, GithubActionsRepo, GithubActionsWorkflowPath, Status string
+		TotalDurationSeconds                                                     int64
+	}
+	for interval, measure := range map[string]*stats.Int64Measure{
+		"1 hour": v2metrics.GithubActions1HourTotalDurationMeasure,
+		"7 days": v2metrics.GithubActions7DayTotalDurationMeasure,
+	} {
+		for retryLabel, attemptCountQuery := range map[string]string{
+			"false": "= 1",
+			"true":  "> 1",
+		} {
+			var results []TotalDurationResult
+			if err := db.Raw(fmt.Sprintf(`
+select v2_ci_runs.github_actions_owner,
+       v2_ci_runs.github_actions_repo,
+       v2_ci_runs.github_actions_workflow_path,
+       v2_ci_runs.status,
+       round(sum(extract(epoch from v2_ci_runs.terminal_at - v2_ci_runs.started_at)))::bigint as total_duration_seconds
+from v2_ci_runs
+where v2_ci_runs.platform = 'github-actions'
+  and v2_ci_runs.terminal_at >= current_timestamp - '%s'::interval
+  and v2_ci_runs.started_at is not null
+  and v2_ci_runs.github_actions_attempt_number %s
+group by v2_ci_runs.github_actions_owner,
+         v2_ci_runs.github_actions_repo,
+         v2_ci_runs.github_actions_workflow_path,
+         v2_ci_runs.status
+`, interval, attemptCountQuery)).Scan(&results).Error; err != nil {
+				return err
+			}
+			ctx, err := tag.New(ctx,
+				tag.Upsert(v2metrics.GithubActionsRetryKey, retryLabel))
+			if err != nil {
+				return err
+			}
+			for _, result := range results {
+				ctx, err := tag.New(ctx,
+					tag.Upsert(v2metrics.GithubActionsRepoKey, fmt.Sprintf("%s/%s", result.GithubActionsOwner, result.GithubActionsRepo)),
+					tag.Upsert(v2metrics.GithubActionsWorkflowFileKey, result.GithubActionsWorkflowPath),
+					tag.Upsert(v2metrics.GithubActionsOutcomeKey, result.Status))
+				if err != nil {
+					return err
+				}
+				stats.Record(ctx, measure.M(result.TotalDurationSeconds))
 			}
 		}
 	}
@@ -361,6 +463,10 @@ func UpdateMetrics(ctx context.Context, db *gorm.DB) error {
 	}
 
 	if err = reportEnvironmentStateCounts(ctx, db); err != nil {
+		return err
+	}
+
+	if err = reportGitHubActionMetrics(ctx, db); err != nil {
 		return err
 	}
 
