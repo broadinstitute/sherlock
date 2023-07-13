db28dc0951e39053a8a7621a7f8ce3e84e53d8db
Jack Warren <jack@jackwarren.info>
2023-07-12 21:28:23 -0400

    some db tests

--- sherlock/internal/db/db_connect_test.go
+++ sherlock/internal/db/db_connect_test.go
@@ -1,9 +1,33 @@
 package db
 
 import (
+	"github.com/broadinstitute/sherlock/go-shared/pkg/testutils"
+	"github.com/broadinstitute/sherlock/sherlock/internal/config"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm/logger"
 	"testing"
 )
+
+func TestConnectAndConfigure(t *testing.T) {
+	config.LoadTestConfig()
+	sqlDB, err := Connect()
+	assert.NoError(t, err)
+	assert.NotNil(t, sqlDB)
+	gormDB, err := Configure(sqlDB)
+	assert.NoError(t, err)
+	assert.NotNil(t, gormDB)
+	sqlDB, err = gormDB.DB()
+	assert.NoError(t, err)
+	assert.NotNil(t, sqlDB)
+	err = sqlDB.Close()
+	assert.NoError(t, err)
+}
+
+func Test_dbConnectionString(t *testing.T) {
+	config.LoadTestConfig()
+	s := dbConnectionString()
+	testutils.AssertNoDiff(t, "postgres://sherlock:password@localhost:5432/sherlock?sslmode=disable", s)
+}
 
 func Test_parseGormLogLevel(t *testing.T) {
 	type args struct {
