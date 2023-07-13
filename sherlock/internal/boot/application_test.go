0a5173ef904017a263dc6199e200c2b1e0e3dd61
Jack Warren <jack@jackwarren.info>
2023-07-12 23:40:17 -0400

    as one test?

--- sherlock/internal/boot/application_test.go
+++ sherlock/internal/boot/application_test.go
@@ -13,26 +13,34 @@ func TestApplication_StartStop(t *testing.T) {
 	application := &Application{
 		runInsideDatabaseTransaction: true,
 	}
-	go application.Start()
-	var livenessSucceeded, readinessSucceeded bool
-	attemptsRemaining := 4 * 20
-	for ; attemptsRemaining >= 0 && !livenessSucceeded; attemptsRemaining-- {
-		resp, err := http.Get("http://localhost:8081")
-		if err == nil && resp.StatusCode == 200 {
-			livenessSucceeded = true
-		} else {
-			time.Sleep(time.Second / 4)
+
+	t.Run("stop", func(t *testing.T) {
+		assert.NotPanics(t, application.Stop)
+	})
+
+	t.Run("start and then stop", func(t *testing.T) {
+		go application.Start()
+		var livenessSucceeded, readinessSucceeded bool
+		attemptsRemaining := 4 * 20
+		for ; attemptsRemaining >= 0 && !livenessSucceeded; attemptsRemaining-- {
+			resp, err := http.Get("http://localhost:8081")
+			if err == nil && resp.StatusCode == 200 {
+				livenessSucceeded = true
+			} else {
+				time.Sleep(time.Second / 4)
+			}
 		}
-	}
-	for ; attemptsRemaining >= 0 && !readinessSucceeded; attemptsRemaining-- {
-		resp, err := http.Get("http://localhost:8080/status")
-		if err == nil && resp.StatusCode == 200 {
-			readinessSucceeded = true
-		} else {
-			time.Sleep(time.Second / 4)
+		for ; attemptsRemaining >= 0 && !readinessSucceeded; attemptsRemaining-- {
+			resp, err := http.Get("http://localhost:8080/status")
+			if err == nil && resp.StatusCode == 200 {
+				readinessSucceeded = true
+			} else {
+				time.Sleep(time.Second / 4)
+			}
 		}
-	}
-	application.Stop()
-	assert.Truef(t, livenessSucceeded, ":8081 returned 200")
-	assert.Truef(t, readinessSucceeded, ":8080/status returned 200")
+		assert.NotPanics(t, application.Stop)
+		assert.Truef(t, livenessSucceeded, ":8081 returned 200")
+		assert.Truef(t, readinessSucceeded, ":8080/status returned 200")
+	})
+
 }
