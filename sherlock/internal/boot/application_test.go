a88128b0527b2c95975bc20f339f3c67e3ebb941
Jack Warren <jack@jackwarren.info>
2023-07-27 14:21:44 -0400

    [DDO-2991] Block shutdown during migrations (#221)
    * block shutdown during migrations
    
    * test
    
    * fix lint
    
    * suite for tests?
    
    * fix race?
    
    * fix flake

--- sherlock/internal/boot/application_test.go
+++ sherlock/internal/boot/application_test.go
@@ -2,19 +2,32 @@ package boot
 
 import (
 	"github.com/broadinstitute/sherlock/sherlock/internal/config"
-	"github.com/stretchr/testify/assert"
+	"github.com/broadinstitute/sherlock/sherlock/internal/db"
+	"github.com/stretchr/testify/suite"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 )
 
-func TestApplication_StartStop(t *testing.T) {
+type applicationSuite struct {
+	suite.Suite
+}
+
+func TestApplicationSuite(t *testing.T) {
+	suite.Run(t, new(applicationSuite))
+}
+
+func (s *applicationSuite) SetupSuite() {
 	config.LoadTestConfig()
+}
+
+func (s *applicationSuite) TestApplication_StartStop() {
 	application := &Application{
 		runInsideDatabaseTransaction: true,
 	}
 
-	t.Run("start and then stop", func(t *testing.T) {
+	s.Run("start and then stop", func() {
 		go application.Start()
 		var livenessSucceeded, readinessSucceeded bool
 		attemptsRemaining := 4 * 20
@@ -35,7 +48,52 @@ func TestApplication_StartStop(t *testing.T) {
 			}
 		}
 		application.Stop()
-		assert.Truef(t, livenessSucceeded, ":8081 returned 200")
-		assert.Truef(t, readinessSucceeded, ":8080/status returned 200")
+		s.Truef(livenessSucceeded, ":8081 returned 200")
+		s.Truef(readinessSucceeded, ":8080/status returned 200")
 	})
+}
+
+func (s *applicationSuite) TestApplication_dbMigrationLock() {
+	sqlDB, err := db.Connect()
+	s.NoError(err)
+	application := &Application{
+		sqlDB: sqlDB,
+	}
+	// Pretend that a migration is ongoing
+	application.dbMigrationLock.Lock()
+
+	// We want to block until the Stop() goroutine finishes, so we'll have it unlock this mutex
+	completedMarker := sync.Mutex{}
+	completedMarker.Lock()
+
+	// We want to know that the Stop() call blocks until it shouldn't, so we have a boolean
+	// with a mutex around it so that we can change whether the assertion passes or fails from
+	// outside.
+	completionDesired := false
+	completionDesiredMutex := sync.Mutex{}
+	go func() {
+		application.Stop()
+		completionDesiredMutex.Lock()
+		s.True(completionDesired)
+		completionDesiredMutex.Unlock()
+
+		// Unlock this to indicate that the test can exit
+		completedMarker.Unlock()
+	}()
+
+	// Sleep so that we make sure the goroutine should be blocking on Stop()
+	time.Sleep(time.Second)
+
+	// Pretend that the migration just completed
+	completionDesiredMutex.Lock()
+	application.dbMigrationLock.Unlock()
+	completionDesired = true
+	completionDesiredMutex.Unlock()
+
+	// Block for completion of the goroutine
+	completedMarker.Lock()
+
+	// Have to make the linter stand down
+	//nolint:staticcheck
+	completedMarker.Unlock()
 }
