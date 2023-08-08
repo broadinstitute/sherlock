e59061284ae595aee01cf600d87cb895060c5670
Jack Warren <jack@jackwarren.info>
2023-08-08 18:48:35 -0400

    slack_deploy_hook_v3

--- sherlock/internal/api/sherlock/slack_deploy_hook_v3.go
+++ sherlock/internal/api/sherlock/slack_deploy_hook_v3.go
@@ -1,5 +1,30 @@
 package sherlock
 
+import (
+	"github.com/broadinstitute/sherlock/sherlock/internal/models"
+	"gorm.io/gorm"
+)
+
 type SlackDeployHookV3 struct {
 	commonFields
+	deployHookTriggerConfigV3
+	SlackChannel *string
+}
+
+func (s SlackDeployHookV3) toModel(db *gorm.DB) (models.SlackDeployHook, error) {
+	ret := models.SlackDeployHook{
+		Model:        s.commonFields.toGormModel(),
+		SlackChannel: s.SlackChannel,
+	}
+	var err error
+	ret.Trigger, err = s.deployHookTriggerConfigV3.toModel(db)
+	return ret, err
+}
+
+func slackDeployHookFromModel(model models.SlackDeployHook) SlackDeployHookV3 {
+	return SlackDeployHookV3{
+		commonFields:              commonFieldsFromGormModel(model.Model),
+		deployHookTriggerConfigV3: deployHookTriggerConfigFromModel(model.Trigger),
+		SlackChannel:              model.SlackChannel,
+	}
 }
