efac1b3698ea3a8cdcc052e205bae56344cfa2ce
Jack Warren <jack@jackwarren.info>
2023-08-15 16:10:27 -0400

    [DDO-3041] Remove v1 tables (#249)

---
+++ sherlock/db/migrations/000049_drop_v1_tables.up.sql
@@ -0,0 +1,28 @@
+-- 000007_create_allocation_pools_table.down.sql
+
+DROP TABLE IF EXISTS allocation_pools CASCADE;
+
+-- 000006_create_clusters_table.down.sql
+
+DROP TABLE IF EXISTS clusters CASCADE;
+
+-- 000005_create_deploys_table.down.sql
+
+DROP TABLE IF EXISTS deploys CASCADE;
+
+-- 000004_create_builds_table.down.sql
+
+DROP TABLE IF EXISTS builds CASCADE;
+
+-- 000003_create_service_instances_table.down.sql
+
+DROP TABLE IF EXISTS service_instances CASCADE;
+
+-- 000002_create_services_table.down.sql
+
+DROP TABLE IF EXISTS services CASCADE;
+
+-- 000001_create_environments_table.down.sql
+
+DROP TABLE IF EXISTS environments CASCADE;
+
