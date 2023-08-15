07a8f830d03e37da34e0ccd35065871e7b8d54eb
Jack Warren <jack@jackwarren.info>
2023-08-15 11:17:44 -0400

    make function internal

--- sherlock/internal/errors/abort.go
+++ sherlock/internal/errors/abort.go
@@ -5,5 +5,5 @@ import "github.com/gin-gonic/gin"
 // AbortRequest abstracts the specific incantation needed to correctly return an errors.ErrorResponse
 // from a handler.
 func AbortRequest(ctx *gin.Context, err error) {
-	ctx.AbortWithStatusJSON(ErrorToApiResponse(ctx.Error(err)))
+	ctx.AbortWithStatusJSON(errorToApiResponse(ctx.Error(err)))
 }
