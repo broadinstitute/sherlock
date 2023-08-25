c291edde60adfefee319eb544f5702ca5683656f
Jack Warren <jack@jackwarren.info>
2023-08-25 12:17:01 -0400

    fixes

--- sherlock/internal/github/dispatch_workflow.go
+++ sherlock/internal/github/dispatch_workflow.go
@@ -3,15 +3,19 @@ package github
 import (
 	"context"
 	"github.com/google/go-github/v50/github"
+	"strings"
 )
 
 // DispatchWorkflow basically wraps client.Actions.CreateWorkflowDispatchEventByFileName.
 // This is so that we can mock it and to simplify imports (the caller doesn't need to
 // import github.CreateWorkflowDispatchEventRequest, which clashes with our own package
-// name)
+// name).
+// We can also handle the case where GitHub's own API refers to workflow file paths
+// differently: here we always want to pass just the filename.
 func DispatchWorkflow(ctx context.Context, owner string, repo string, workflowPath string, gitRef string, inputs map[string]any) error {
+	workflowPathSplit := strings.Split(workflowPath, "/")
 	_, err := client.Actions.CreateWorkflowDispatchEventByFileName(
-		ctx, owner, repo, workflowPath, github.CreateWorkflowDispatchEventRequest{
+		ctx, owner, repo, workflowPathSplit[len(workflowPathSplit)-1], github.CreateWorkflowDispatchEventRequest{
 			Ref:    gitRef,
 			Inputs: inputs,
 		})
