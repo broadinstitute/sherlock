0aa87e5e881cac9fc6c6c0087be072608edefc64
Katie Welch <katie.welch70@yahoo.com>
2023-10-05 17:12:46 -0400

    add upsert test

--- sherlock/internal/api/sherlock/app_version_v3_upsert_test.go
+++ sherlock/internal/api/sherlock/app_version_v3_upsert_test.go
@@ -1,7 +1,9 @@
 package sherlock
 
 import (
+	"github.com/broadinstitute/sherlock/go-shared/pkg/utils"
 	"github.com/broadinstitute/sherlock/sherlock/internal/errors"
+	"github.com/broadinstitute/sherlock/sherlock/internal/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -38,19 +40,53 @@ func (s *handlerSuite) TestAppVersionsV3Upsert_error() {
 }
 
 func (s *handlerSuite) TestAppVersionsV3Upsert() {
-	var got ChartV3
+	chart := models.Chart{
+		Name:      "chart-name",
+		ChartRepo: utils.PointerTo("terra-helm"),
+	}
+	s.NoError(s.DB.Create(&chart).Error)
+
+	var got AppVersionV3
 	code := s.HandleRequest(
-		s.NewRequest("POST", "/api/charts/v3", ChartV3Create{
-			Name: "chart-name",
+		s.NewRequest("PUT", "/api/app-versions/v3", AppVersionV3Create{
+			Chart:            "chart-name",
+			AppVersion:       "2",
+			GitCommit:        "123",
+			GitBranch:        "branch",
+			ParentAppVersion: "1",
+			AppVersionV3Edit: AppVersionV3Edit{
+				Description: "original description",
+			},
 		}),
 		&got)
 	s.Equal(http.StatusCreated, code)
-	s.Equal("chart-name", got.Name)
-	if s.NotNil(got.ChartRepo) {
-		s.Equal("terra-helm", *got.ChartRepo)
+	s.Equal("chart-name", got.Chart)
+	if s.NotNil(got.GitCommit) {
+		s.Equal("123", got.GitCommit)
 	}
-	if s.NotNil(got.DefaultSubdomain) {
-		s.Equal("chart-name", *got.DefaultSubdomain)
+	if s.NotNil(got.GitBranch) {
+		s.Equal("branch", got.GitBranch)
 	}
-	// check that i can edit description, specify git branch, but it doesn't actually edit
+	if s.NotNil(got.Description) {
+		s.Equal("original description", got.Description)
+	}
+
+	var got2 AppVersionV3
+	code = s.HandleRequest(
+		s.NewRequest("PUT", "/api/app-versions/v3", AppVersionV3Create{
+			Chart:     "chart-name",
+			GitCommit: "456",
+			AppVersionV3Edit: AppVersionV3Edit{
+				Description: "edited description",
+			},
+		}),
+		&got2)
+	s.Equal(http.StatusCreated, code)
+	s.Equal("chart-name", got2.Chart)
+	if s.NotNil(got2.Description) {
+		s.Equal("edited description", got2.Description)
+	}
+	if s.NotNil(got2.GitCommit) {
+		s.Equal("123", got2.GitCommit)
+	}
 }
