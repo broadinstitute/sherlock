9033a01af756448adfba81acc88c7914c3ebc331
Jack Warren <jack@jackwarren.info>
2023-10-31 10:42:20 -0400

    [DDO-3273] Migrate PagerdutyIntegration type (#344)
    Co-authored-by: Katie Welch <katie.welch70@yahoo.com>

--- sherlock/internal/models/pagerduty_integration.go
+++ sherlock/internal/models/pagerduty_integration.go
@@ -10,6 +10,31 @@ type PagerdutyIntegration struct {
 	Type        *string
 }
 
-func (p PagerdutyIntegration) TableName() string {
+func (p *PagerdutyIntegration) TableName() string {
 	return "pagerduty_integrations"
+}
+
+func (p *PagerdutyIntegration) errorIfForbidden(tx *gorm.DB) error {
+	if user, err := GetCurrentUserForDB(tx); err != nil {
+		return err
+	} else if err = user.Suitability().SuitableOrError(); err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+// BeforeCreate checks permissions
+func (p *PagerdutyIntegration) BeforeCreate(tx *gorm.DB) error {
+	return p.errorIfForbidden(tx)
+}
+
+// BeforeUpdate checks permissions
+func (p *PagerdutyIntegration) BeforeUpdate(tx *gorm.DB) error {
+	return p.errorIfForbidden(tx)
+}
+
+// BeforeDelete checks permissions
+func (p *PagerdutyIntegration) BeforeDelete(tx *gorm.DB) error {
+	return p.errorIfForbidden(tx)
 }
