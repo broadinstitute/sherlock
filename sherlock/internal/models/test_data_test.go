22cf9f2c4e9a62694f54978ffeb01024cbe0f97b
Jack Warren <jack@jackwarren.info>
2023-11-07 21:43:34 -0500

    improve metrics testing

--- sherlock/internal/models/test_data_test.go
+++ sherlock/internal/models/test_data_test.go
@@ -1,15 +1,16 @@
 package models
 
 import (
+	"context"
+	"fmt"
 	"math/rand"
 	"reflect"
 	"time"
 )
 
-// TestTestDataItself iterates through the TestData's underlying type and executes each
-// method in a random order. This helps flag when the issue is with the TestData and not
-// the test calling it.
-func (s *modelSuite) TestTestDataItself() {
+// TestBulkData uses reflection to iterate through all available methods on TestData, followed
+// by a run of UpdateMetrics to at least validate that the batch metrics cron doesn't error
+func (s *modelSuite) TestBulkData() {
 	testDataValue := reflect.ValueOf(s.TestData)
 	// Rather than iterating through the methods in order, use rand.Perm to effectively
 	// shuffle the order we process each index.
@@ -19,11 +20,14 @@ func (s *modelSuite) TestTestDataItself() {
 		methodTypeValue := testDataValue.Type().Method(i)
 		// Method's receiver counts as an argument
 		if methodTypeValue.Type.NumIn() == 1 {
-			s.Run(methodTypeValue.Name, func() {
+			s.Run(fmt.Sprintf("run TestData.%s()", methodTypeValue.Name), func() {
 				// Call the method accessed from the Value directly, not the Type's Value,
 				// so we don't have to pass the receiver ourselves
 				testDataValue.Method(i).Call([]reflect.Value{})
 			})
 		}
 	}
+	s.Run("run batch metrics cron", func() {
+		s.NoError(UpdateMetrics(context.Background(), s.DB))
+	})
 }
