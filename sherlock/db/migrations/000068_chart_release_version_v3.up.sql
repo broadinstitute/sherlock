4f906726b8c6c7697955fa8ac746b1590f1677f1
Katie Welch <katie.welch70@yahoo.com>
2023-12-12 11:53:57 -0500

    changes

--- sherlock/db/migrations/000068_chart_release_version_v3.up.sql
+++ sherlock/db/migrations/000068_chart_release_version_v3.up.sql
@@ -9,21 +9,21 @@ alter table chart_releases
                (((app_version_resolver = 'branch' and
                 app_version_branch is not null and app_version_branch != '' and
                 app_version_id is not null and
-                public.app_versions.git_branch = app_version_branch and
+                /*public.app_versions.git_branch = app_version_branch and*/
                 app_version_commit is not null and app_version_commit != '' and
-                public.app_versions.git_commit = app_version_commit and
-                app_version_exact is not null and app_version_exact != '' and
-                public.app_versions.app_version = app_version_exact) or
+                /*public.app_versions.git_commit = app_version_commit and*/
+                app_version_exact is not null and app_version_exact != '' /*and
+                public.app_versions.app_version = app_version_exact*/) or
 
                (app_version_resolver = 'commit' and
                 app_version_commit is not null and app_version_commit != '' and
-                public.app_versions.git_commit = app_version_commit and
-                app_version_exact is not null and app_version_exact != '' and
-                public.app_versions.app_version = app_version_exact) or
+                /*public.app_versions.git_commit = app_version_commit and*/
+                app_version_exact is not null and app_version_exact != '' /*and
+                public.app_versions.app_version = app_version_exact*/) or
 
                (app_version_resolver = 'exact' and
-                app_version_exact is not null and app_version_exact != '' and
-                public.app_versions.app_version = app_version_exact) or
+                app_version_exact is not null and app_version_exact != '' /*and
+                public.app_versions.app_version = app_version_exact*/) or
 
                (app_version_resolver = 'follow' and app_version_follow_chart_release_id is not null) or
 
@@ -34,8 +34,8 @@ alter table chart_releases
                 app_version_id is null and
                 app_version_follow_chart_release_id is null)) and
 
-               (app_version_id is null or
-                public.app_versions.id = app_version_id)));
+               (/*app_version_id is null or
+                public.app_versions.id = app_version_id*/)));
 
 alter table chart_releases
     add constraint chart_version_resolver_valid
@@ -43,18 +43,18 @@ alter table chart_releases
                 chart_version_exact is not null and chart_version_exact != '') and
 
                (((chart_version_resolver = 'latest' and
-                 chart_version_id is not null and
-                 public.chart_versions.chart_version = chart_version_exact) or
+                 chart_version_id is not null /*and
+                 public.chart_versions.chart_version = chart_version_exact*/) or
 
                 (chart_version_resolver = 'exact' and
-                 (chart_version_id is null or
-                  public.chart_versions.chart_version = chart_version_exact)) or
+                 (chart_version_id is null /*or
+                  public.chart_versions.chart_version = chart_version_exact*/)) or
 
                 (chart_version_resolver = 'follow' and
                  chart_version_follow_chart_release_id is not null)) and
 
-                (chart_version_id is null or
-                 public.chart_versions.id = chart_version_id)));
+                (chart_version_id is null /*or
+                 public.chart_versions.id = chart_version_id*/)));
 
 alter table chart_releases
     add constraint helmfile_ref_valid
--- sherlock/internal/models/chart_release_version_test.go
+++ sherlock/internal/models/chart_release_version_test.go
@@ -2,7 +2,8 @@ package models
 
 func (s *modelSuite) TestChartReleaseVersionResolvedAtPresent() {
 	s.SetSuitableTestUserForDB()
-	chartReleaseVersion := s.TestData.ChartRelease_LeonardoDev().ChartVersion
+	chartRelease := s.TestData.ChartRelease_LeonardoDev()
+	chartReleaseVersion := chartRelease.ChartReleaseVersion
 	err := s.DB.Model(&chartReleaseVersion).Select("ResolvedAt").Updates(&ChartReleaseVersion{ResolvedAt: nil}).Error
 	s.ErrorContains(err, "violates check constraint \"resolved_at_present\"")
 }
