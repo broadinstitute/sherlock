12052f8eb6f173b04bdd48cb62a5746ff823f9e8
Katie <welchkat@broadinstitute.org>
2024-01-08 16:57:25 -0500

    DDO-3377: Handle Committed At time is 0 (#427)
    * if committed at is 0
    
    * change to nil
    
    * simiplify

--- sherlock/internal/api/sherlock/git_commits_v3_upsert.go
+++ sherlock/internal/api/sherlock/git_commits_v3_upsert.go
@@ -53,7 +53,7 @@ func gitCommitsV3Upsert(ctx *gin.Context) {
 
 	var timeSince *uint
 
-	if len(previous) > 0 {
+	if len(previous) > 0 && !previous[0].CommittedAt.IsZero() && !body.CommittedAt.IsZero() {
 		timeSince = utils.PointerTo(uint(body.CommittedAt.Sub(previous[0].CommittedAt).Seconds()))
 	}
 
