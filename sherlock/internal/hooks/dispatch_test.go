c0a82459feb350116a17550d19356666c73560cc
Jack Warren <jack@jackwarren.info>
2024-01-19 15:31:27 -0500

    [DDO-3430] Deduplicate deploy hook triggers (#441)

--- sherlock/internal/hooks/dispatch_test.go
+++ sherlock/internal/hooks/dispatch_test.go
@@ -42,6 +42,64 @@ func (s *hooksSuite) TestDispatch_hooks() {
 	})
 }
 
+func (s *hooksSuite) TestDispatch_duplicateHooks() {
+	ciRun := s.TestData.CiRun_Deploy_LeonardoDev_V1toV3()
+
+	// Create two Slack hooks
+	slackHook := s.TestData.SlackDeployHook_Dev()
+	s.NoError(s.DB.Create(&models.SlackDeployHook{
+		Trigger: models.DeployHookTriggerConfig{
+			OnEnvironmentID: slackHook.Trigger.OnEnvironmentID,
+			OnSuccess:       slackHook.Trigger.OnSuccess,
+			OnFailure:       slackHook.Trigger.OnFailure,
+		},
+		SlackChannel:  slackHook.SlackChannel,
+		MentionPeople: slackHook.MentionPeople,
+	}).Error)
+
+	// Create two Github Actions hooks
+	ghaHook := s.TestData.GithubActionsDeployHook_LeonardoDev()
+	s.NoError(s.DB.Create(&models.GithubActionsDeployHook{
+		Trigger: models.DeployHookTriggerConfig{
+			OnChartReleaseID: ghaHook.Trigger.OnChartReleaseID,
+			OnSuccess:        ghaHook.Trigger.OnSuccess,
+			OnFailure:        ghaHook.Trigger.OnFailure,
+		},
+		GithubActionsOwner:          ghaHook.GithubActionsOwner,
+		GithubActionsRepo:           ghaHook.GithubActionsRepo,
+		GithubActionsWorkflowPath:   ghaHook.GithubActionsWorkflowPath,
+		GithubActionsDefaultRef:     ghaHook.GithubActionsDefaultRef,
+		GithubActionsRefBehavior:    ghaHook.GithubActionsRefBehavior,
+		GithubActionsWorkflowInputs: ghaHook.GithubActionsWorkflowInputs,
+	}).Error)
+
+	completionText, errs := ciRun.SlackCompletionText(s.DB)
+	s.Empty(errs)
+	slack.UseMockedClient(s.T(), func(c *slack_mocks.MockMockableClient) {}, func() {
+		UseMockedDispatcher(s.T(), func(d *hooks_mocks.MockMockableDispatcher) {
+			for _, channel := range ciRun.NotifySlackChannelsUponSuccess {
+				d.EXPECT().DispatchSlackCompletionNotification(
+					mock.Anything,
+					channel,
+					completionText,
+					ciRun.Succeeded(),
+					ciRun.NotifySlackCustomIcon).
+					Return(nil).Once()
+			}
+			d.EXPECT().DispatchSlackDeployHook(
+				mock.Anything,
+				mock.Anything,
+				ciRun).Return(nil).Once()
+			d.EXPECT().DispatchGithubActionsDeployHook(
+				mock.Anything,
+				mock.Anything,
+				ciRun).Return(nil).Once()
+		}, func() {
+			Dispatch(s.DB, ciRun)
+		})
+	})
+}
+
 func (s *hooksSuite) TestDispatch_completionMessageWithIcon() {
 	ciRun := s.TestData.CiRun_Deploy_LeonardoDev_V1toV3()
 	s.NoError(s.DB.Model(&ciRun).Updates(&models.CiRun{NotifySlackCustomIcon: utils.PointerTo(":smiley:")}).Error)
