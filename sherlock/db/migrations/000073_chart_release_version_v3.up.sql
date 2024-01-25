8125e6ef5532dbea2e0b383d7ca767fa3f532d59
Katie Welch <katie.welch70@yahoo.com>
2024-01-25 15:40:04 -0500

    update

--- sherlock/db/migrations/000073_chart_release_version_v3.up.sql
+++ sherlock/db/migrations/000073_chart_release_version_v3.up.sql
@@ -9,33 +9,24 @@ alter table chart_releases
                (((app_version_resolver = 'branch' and
                 app_version_branch is not null and app_version_branch != '' and
                 app_version_id is not null and
-                /*public.app_versions.git_branch = app_version_branch and*/
                 app_version_commit is not null and app_version_commit != '' and
-                /*public.app_versions.git_commit = app_version_commit and*/
-                app_version_exact is not null and app_version_exact != '' /*and
-                public.app_versions.app_version = app_version_exact*/) or
+                app_version_exact is not null and app_version_exact != '') or
 
                (app_version_resolver = 'commit' and
                 app_version_commit is not null and app_version_commit != '' and
-                /*public.app_versions.git_commit = app_version_commit and*/
-                app_version_exact is not null and app_version_exact != '' /*and
-                public.app_versions.app_version = app_version_exact*/) or
+                app_version_exact is not null and app_version_exact != '') or
 
                (app_version_resolver = 'exact' and
-                app_version_exact is not null and app_version_exact != '' /*and
-                public.app_versions.app_version = app_version_exact*/) or
+                app_version_exact is not null and app_version_exact != '') or
 
                (app_version_resolver = 'follow' and app_version_follow_chart_release_id is not null) or
 
                (app_version_resolver = 'none' and
-                (app_version_branch is null or app_version_branch = '') and
-                (app_version_commit is null or app_version_commit = '') and
-                (app_version_exact is null or app_version_exact = '') and
+                (app_version_branch is null and app_version_branch = '') and
+                (app_version_commit is null and app_version_commit = '') and
+                (app_version_exact is null and app_version_exact = '') and
                 app_version_id is null and
-                app_version_follow_chart_release_id is null)) and
-
-               (/*app_version_id is null or
-                public.app_versions.id = app_version_id*/)));
+                app_version_follow_chart_release_id is null))));
 
 alter table chart_releases
     add constraint chart_version_resolver_valid
@@ -43,18 +34,13 @@ alter table chart_releases
                 chart_version_exact is not null and chart_version_exact != '') and
 
                (((chart_version_resolver = 'latest' and
-                 chart_version_id is not null /*and
-                 public.chart_versions.chart_version = chart_version_exact*/) or
+                 chart_version_id is not null) or
 
                 (chart_version_resolver = 'exact' and
-                 (chart_version_id is null /*or
-                  public.chart_versions.chart_version = chart_version_exact*/)) or
+                 (chart_version_id is null)) or
 
                 (chart_version_resolver = 'follow' and
-                 chart_version_follow_chart_release_id is not null)) and
-
-                (chart_version_id is null /*or
-                 public.chart_versions.id = chart_version_id*/)));
+                 chart_version_follow_chart_release_id is not null))));
 
 alter table chart_releases
     add constraint helmfile_ref_valid
