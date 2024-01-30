141b9008e66c0397f32d0718bbf5b2bde6a86a98
Jack Warren <jack@jackwarren.info>
2024-01-29 19:53:41 -0500

    cr delete

---
+++ sherlock/internal/api/sherlock/chart_releases_v3_delete.go
@@ -0,0 +1,42 @@
+package sherlock
+
+import (
+	"github.com/broadinstitute/sherlock/sherlock/internal/authentication"
+	"github.com/broadinstitute/sherlock/sherlock/internal/errors"
+	"github.com/broadinstitute/sherlock/sherlock/internal/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm/clause"
+	"net/http"
+)
+
+// chartReleasesV3Delete godoc
+//
+//	@summary		Delete an individual ChartRelease
+//	@description	Delete an individual ChartRelease by its ID.
+//	@tags			ChartReleases
+//	@produce		json
+//	@param			selector				path		string	true	"The selector of the ChartRelease, which can be either a numeric ID, the name, environment + '/' + chart, or cluster + '/' + namespace + '/' + chart."
+//	@success		200						{object}	ChartReleaseV3
+//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
+//	@router			/api/chart-releases/v3/{selector} [delete]
+func chartReleasesV3Delete(ctx *gin.Context) {
+	db, err := authentication.MustUseDB(ctx)
+	if err != nil {
+		return
+	}
+	query, err := chartReleaseModelFromSelector(db, canonicalizeSelector(ctx.Param("selector")))
+	if err != nil {
+		errors.AbortRequest(ctx, err)
+		return
+	}
+	var result models.ChartRelease
+	if err = db.Preload(clause.Associations).Where(&query).First(&result).Error; err != nil {
+		errors.AbortRequest(ctx, err)
+		return
+	}
+	if err = db.Delete(&result).Error; err != nil {
+		errors.AbortRequest(ctx, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, chartReleaseFromModel(result))
+}
