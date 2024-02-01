03d0b80512e6f3be1a58f863a8b2d63e2352af3b
Jack Warren <jack@jackwarren.info>
2024-02-01 13:25:27 -0500

    db migration

---
+++ sherlock/db/migrations/000076_block_duplicate_chart_releases.down.sql
@@ -0,0 +1,5 @@
+drop index if exists chart_releases_name_unique;
+
+drop index if exists chart_releases_environment_chart_unique;
+
+drop index if exists chart_releases_cluster_namespace_chart_unique;
