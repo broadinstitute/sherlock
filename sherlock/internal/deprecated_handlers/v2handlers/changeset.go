4087836536bcca702f2f06e6c8a34aeb0cf87e41
Jack Warren <jack@jackwarren.info>
2024-02-13 17:38:11 -0500

    remove OpenAPI docs for v2

--- sherlock/internal/deprecated_handlers/v2handlers/changeset.go
+++ sherlock/internal/deprecated_handlers/v2handlers/changeset.go
@@ -21,60 +21,18 @@ func registerChangesetHandlers(routerGroup *gin.RouterGroup, controller *v2contr
 	routerGroup.GET("/procedures/changesets/query-applied-for-version/:version-type/:chart/:version", queryAppliedChangesetForVersion(controller))
 }
 
-// listChangeset godoc
-//
-//	@summary		List Changeset entries
-//	@description	List existing Changeset entries, ordered by most recently updated.
-//	@tags			Changesets
-//	@produce		json
-//	@param			filter					query		v2controllers.Changeset	false	"Optional filters to apply to the returned entries"
-//	@param			limit					query		int						false	"An optional limit to the number of entries returned"
-//	@success		200						{array}		v2controllers.Changeset
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/changesets [get]
 func listChangeset(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return handleList(&controller.ModelController)
 }
 
-// getChangeset godoc
-//
-//	@summary		Get a Changeset entry
-//	@description	Get an existing Changeset entry via its "selector"--its numeric ID.
-//	@tags			Changesets
-//	@produce		json
-//	@param			selector				path		string	true	"The Changeset to get's selector--numeric ID"
-//	@success		200						{object}	v2controllers.Changeset
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/changesets/{selector} [get]
 func getChangeset(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return handleGet(&controller.ModelController)
 }
 
-// listChangesetSelectors godoc
-//
-//	@summary		List Changeset selectors
-//	@description	Validate a given Changeset selector and provide any other selectors that would match the same Changeset.
-//	@tags			Changesets
-//	@produce		json
-//	@param			selector				path		string	true	"The selector of the Changeset to list other selectors for"
-//	@success		200						{array}		string
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/selectors/changesets/{selector} [get]
 func listChangesetSelectors(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return handleSelectorList(&controller.ModelController)
 }
 
-// planAndApplyChangeset godoc
-//
-//	@summary		Plan and apply version changes in one step
-//	@description	Like the plan and apply endpoints immediately in sequence.
-//	@tags			Changesets
-//	@accept			json
-//	@produce		json
-//	@param			changeset-plan-request	body		v2controllers.ChangesetPlanRequest	true	"Info on what version changes or refreshes to perform"
-//	@success		200,201					{array}		v2controllers.Changeset
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/procedures/changesets/plan-and-apply [post]
 func planAndApplyChangeset(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		user, err := authentication.ShouldUseUser(ctx)
@@ -100,18 +58,6 @@ func planAndApplyChangeset(controller *v2controllers2.ChangesetController) func(
 	}
 }
 
-// planChangeset godoc
-//
-//	@summary		Plan--but do not apply--version changes to Chart Releases
-//	@description	Refreshes and calculates version diffs for Chart Releases. If there's a diff, the plan is stored and returned so it can be applied later.
-//	@description	Multiple Chart Releases can be specified--as can groups of Chart Releases from multiple Environments.
-//	@tags			Changesets
-//	@accept			json
-//	@produce		json
-//	@param			changeset-plan-request	body		v2controllers.ChangesetPlanRequest	true	"Info on what version changes or refreshes to plan"
-//	@success		200,201					{array}		v2controllers.Changeset
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/procedures/changesets/plan [post]
 func planChangeset(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		user, err := authentication.ShouldUseUser(ctx)
@@ -137,18 +83,6 @@ func planChangeset(controller *v2controllers2.ChangesetController) func(ctx *gin
 	}
 }
 
-// applyChangeset godoc
-//
-//	@summary		Apply previously planned version changes to Chart Releases
-//	@description	Looks up and applies previously-planned version diffs given by the ID. Other stored plans against the same Chart Releases are marked as superseded.
-//	@description	Multiple Changesets can be specified simply by passing multiple IDs in the list.
-//	@tags			Changesets
-//	@accept			json
-//	@produce		json
-//	@param			apply-request			body		[]string	true	"String IDs of the Changesets to apply"
-//	@success		200						{array}		v2controllers.Changeset
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/procedures/changesets/apply [post]
 func applyChangeset(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		user, err := authentication.ShouldUseUser(ctx)
@@ -170,18 +104,6 @@ func applyChangeset(controller *v2controllers2.ChangesetController) func(ctx *gi
 	}
 }
 
-// queryAppliedChangesetForChartRelease godoc
-//
-//	@summary		List applied Changesets for a Chart Release
-//	@description	List existing applied Changesets for a particular Chart Release, ordered by most recently applied.
-//	@tags			Changesets
-//	@produce		json
-//	@param			selector				path		string	true	"Selector the Chart Release to find applied Changesets for"
-//	@param			offset					query		int		false	"An optional offset to skip a number of latest Changesets"
-//	@param			limit					query		int		false	"An optional limit to the number of entries returned"
-//	@success		200						{array}		v2controllers.Changeset
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/procedures/changesets/query-applied-for-chart-release/{selector} [get]
 func queryAppliedChangesetForChartRelease(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		offsetString := ctx.DefaultQuery("offset", "0")
@@ -205,18 +127,6 @@ func queryAppliedChangesetForChartRelease(controller *v2controllers2.ChangesetCo
 	}
 }
 
-// queryAppliedChangesetForVersion godoc
-//
-//	@summary		List applied Changesets for an App or Chart Version
-//	@description	List existing applied Changesets that newly deployed a given App Version or Chart Version, ordered by most recently applied.
-//	@tags			Changesets
-//	@produce		json
-//	@param			version-type			path		string	true	"The type of the version, either 'app' or 'chart'"	Enums(app, chart)
-//	@param			chart					path		string	true	"The chart the version belongs to"
-//	@param			version					path		string	true	"The version to look for"
-//	@success		200						{array}		v2controllers.Changeset
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/procedures/changesets/query-applied-for-version/{version-type}/{chart}/{version} [get]
 func queryAppliedChangesetForVersion(controller *v2controllers2.ChangesetController) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		if result, err := controller.QueryAppliedForVersion(
--- sherlock/internal/deprecated_handlers/v2handlers/environment.go
+++ sherlock/internal/deprecated_handlers/v2handlers/environment.go
@@ -16,124 +16,34 @@ func registerEnvironmentHandlers(routerGroup *gin.RouterGroup, controller *v2con
 	routerGroup.POST("/procedures/environments/trigger-incident/*selector", triggerEnvironmentPagerdutyIncident(controller))
 }
 
-// createEnvironment godoc
-//
-//	@summary		Create a new Environment entry
-//	@description	Create a new Environment entry. Note that some fields are immutable after creation; /edit lists mutable fields.
-//	@description	Creating a dynamic environment based on a template will also copy ChartReleases from the template.
-//	@tags			Environments
-//	@accept			json
-//	@produce		json
-//	@param			environment				body		v2controllers.CreatableEnvironment	true	"The Environment to create"
-//	@success		200,201					{object}	v2controllers.Environment
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/environments [post]
 func createEnvironment(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleCreate(controller)
 }
 
-// listEnvironment godoc
-//
-//	@summary		List Environment entries
-//	@description	List existing Environment entries, ordered by most recently updated.
-//	@tags			Environments
-//	@produce		json
-//	@param			filter					query		v2controllers.Environment	false	"Optional filters to apply to the returned entries"
-//	@param			limit					query		int							false	"An optional limit to the number of entries returned"
-//	@success		200						{array}		v2controllers.Environment
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/environments [get]
 func listEnvironment(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleList(controller)
 }
 
-// getEnvironment godoc
-//
-//	@summary		Get a Environment entry
-//	@description	Get an existing Environment entry via one of its "selectors": name, numeric ID, or "resource-prefix/" + the unique resource prefix.
-//	@tags			Environments
-//	@produce		json
-//	@param			selector				path		string	true	"The Environment to get's selector: name, numeric ID, or "resource-prefix/" + the unique resource prefix"
-//	@success		200						{object}	v2controllers.Environment
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/environments/{selector} [get]
 func getEnvironment(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleGet(controller)
 }
 
-// editEnvironment godoc
-//
-//	@summary		Edit a Environment entry
-//	@description	Edit an existing Environment entry via one of its "selectors": name, numeric ID. Note that only mutable fields are available here, immutable fields can only be set using /create, or "resource-prefix/" + the unique resource prefix.
-//	@tags			Environments
-//	@accept			json
-//	@produce		json
-//	@param			selector				path		string								true	"The Environment to edit's selector: name, numeric ID, or "resource-prefix/" + the unique resource prefix"
-//	@param			environment				body		v2controllers.EditableEnvironment	true	"The edits to make to the Environment"
-//	@success		200						{object}	v2controllers.Environment
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/environments/{selector} [patch]
 func editEnvironment(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleEdit(controller)
 }
 
-// upsertEnvironment godoc
-//
-//	@summary		Create or edit an Environment entry
-//	@description	Create or edit an Environment entry. Attempts to edit and will attempt to create upon an error.
-//	@description	If an edit was made or the creation process de-duplicates, this method will return normally with a 200.
-//	@tags			Environments
-//	@accept			json
-//	@produce		json
-//	@param			selector				path		string								true	"The Environment to upsert's selector: name or numeric ID"
-//	@param			environment				body		v2controllers.CreatableEnvironment	true	"The Environment to upsert"
-//	@success		200,201					{object}	v2controllers.Environment
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/environments/{selector} [put]
 func upsertEnvironment(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleUpsert(controller)
 }
 
-// deleteEnvironment godoc
-//
-//	@summary		Delete a Environment entry
-//	@description	Delete an existing Environment entry via one of its "selectors": name, numeric ID, or "resource-prefix/" + the unique resource prefix.
-//	@tags			Environments
-//	@produce		json
-//	@param			selector				path		string	true	"The Environment to delete's selector: name, numeric ID, or "resource-prefix/" + the unique resource prefix"
-//	@success		200						{object}	v2controllers.Environment
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/environments/{selector} [delete]
 func deleteEnvironment(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleDelete(controller)
 }
 
-// listEnvironmentSelectors godoc
-//
-//	@summary		List Environment selectors
-//	@description	Validate a given Environment selector and provide any other selectors that would match the same Environment.
-//	@tags			Environments
-//	@produce		json
-//	@param			selector				path		string	true	"The selector of the Environment to list other selectors for"
-//	@success		200						{array}		string
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/selectors/environments/{selector} [get]
 func listEnvironmentSelectors(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleSelectorList(controller)
 }
 
-// triggerEnvironmentPagerdutyIncident godoc
-//
-//	@summary		Trigger a Pagerduty incident for a given Environment
-//	@description	Trigger an alert for the Pagerduty integration configured for a given Environment.
-//	@tags			Environments
-//	@accept			json
-//	@produce		json
-//	@param			selector				path		string					true	"The Environment's selector"
-//	@param			summary					body		pagerduty.AlertSummary	true	"Summary of the incident"
-//	@success		202						{object}	pagerduty.SendAlertResponse
-//	@failure		400,403,404,407,409,500	{object}	errors.ErrorResponse
-//	@router			/api/v2/procedures/environments/trigger-incident/{selector} [post]
 func triggerEnvironmentPagerdutyIncident(controller *v2controllers.EnvironmentController) func(ctx *gin.Context) {
 	return handleTriggerPagerdutyIncident(controller)
 }
