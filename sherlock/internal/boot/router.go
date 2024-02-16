95cb0f66303020370822e0cf96dfd9a841fe9f12
Jack Warren <jack@jackwarren.info>
2024-02-16 12:23:30 -0500

    #minor [DDO-3470] Remove v2 API (#485)

--- sherlock/internal/boot/router.go
+++ sherlock/internal/boot/router.go
@@ -11,7 +11,6 @@ import (
 	"github.com/broadinstitute/sherlock/sherlock/internal/authentication"
 	"github.com/broadinstitute/sherlock/sherlock/internal/boot/middleware"
 	"github.com/broadinstitute/sherlock/sherlock/internal/config"
-	"github.com/broadinstitute/sherlock/sherlock/internal/deprecated_handlers/v2handlers"
 	"github.com/broadinstitute/sherlock/sherlock/internal/errors"
 	"github.com/broadinstitute/sherlock/sherlock/internal/metrics"
 	"github.com/gin-gonic/gin"
@@ -81,8 +80,10 @@ func BuildRouter(ctx context.Context, db *gorm.DB) *gin.Engine {
 	// refactored sherlock API, under /api/{type}/v3
 	sherlock.ConfigureRoutes(apiRouter)
 
-	// deprecated "v2" API, under /api/v2/{type}
-	v2handlers.ConfigureRoutes(apiRouter.Group("v2"), db)
+	// special error for the removed "v2" API, under /api/v2/{type}
+	apiRouter.Any("v2/*path", func(ctx *gin.Context) {
+		errors.AbortRequest(ctx, fmt.Errorf("(%s) sherlock's v2 API has been removed; reach out to #dsp-devops-champions for help updating your client", errors.NotFound))
+	})
 
 	return router
 }
