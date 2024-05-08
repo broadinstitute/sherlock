d6c555c7c5802101826aa99bd02d6c67e8108864
Jack Warren <jack@jackwarren.info>
2024-05-08 12:45:17 -0400

    nil duration

--- sherlock/internal/api/sherlock/roles_v3.go
+++ sherlock/internal/api/sherlock/roles_v3.go
@@ -8,11 +8,11 @@ type RoleV3 struct {
 }
 
 type RoleV3Edit struct {
-	Name                      *string  `json:"name" form:"name"`
-	SuspendNonSuitableUsers   *bool    `json:"suspendNonSuitableUsers,omitempty" form:"suspendNonSuitableUsers"`
-	CanBeGlassBrokenByRole    *uint    `json:"canBeGlassBrokenByRole,omitempty" form:"canBeGlassBrokenByRole"`
-	DefaultGlassBreakDuration Duration `json:"defaultGlassBreakDuration,omitempty" swaggertype:"string" form:"defaultGlassBreakDuration"`
-	GrantsSherlockSuperAdmin  *bool    `json:"grantsSherlockSuperAdmin,omitempty" form:"grantsSherlockSuperAdmin"`
-	GrantsDevFirecloudGroup   *string  `json:"grantsDevFirecloudGroup,omitempty" form:"grantsDevFirecloudGroup"`
-	GrantsDevAzureGroup       *string  `json:"grantsDevAzureGroup,omitempty" form:"grantsDevAzureGroup"`
+	Name                      *string   `json:"name" form:"name"`
+	SuspendNonSuitableUsers   *bool     `json:"suspendNonSuitableUsers,omitempty" form:"suspendNonSuitableUsers"`
+	CanBeGlassBrokenByRole    *uint     `json:"canBeGlassBrokenByRole,omitempty" form:"canBeGlassBrokenByRole"`
+	DefaultGlassBreakDuration *Duration `json:"defaultGlassBreakDuration,omitempty" swaggertype:"string" form:"defaultGlassBreakDuration"`
+	GrantsSherlockSuperAdmin  *bool     `json:"grantsSherlockSuperAdmin,omitempty" form:"grantsSherlockSuperAdmin"`
+	GrantsDevFirecloudGroup   *string   `json:"grantsDevFirecloudGroup,omitempty" form:"grantsDevFirecloudGroup"`
+	GrantsDevAzureGroup       *string   `json:"grantsDevAzureGroup,omitempty" form:"grantsDevAzureGroup"`
 }
