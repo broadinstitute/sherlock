7d903121bae3b4c41b5da7d51b7d560c5189e162
Jack Warren <jack@jackwarren.info>
2024-05-14 11:33:02 -0400

    DDO-3682 treat empty as null for uniqueness checks

---
+++ sherlock/db/migrations/000086_fix_role_uniqueness_constraints.down.sql
@@ -0,0 +1,11 @@
+drop index if exists roles_grants_dev_firecloud_group_unique;
+
+create unique index if not exists roles_grants_dev_firecloud_group_unique
+    on roles (grants_dev_firecloud_group)
+    where deleted_at is null and grants_dev_firecloud_group is not null;
+
+drop index if exists roles_grants_dev_azure_group_unique;
+
+create unique index if not exists roles_grants_dev_azure_group_unique
+    on roles (grants_dev_azure_group)
+    where deleted_at is null and grants_dev_azure_group is not null;
