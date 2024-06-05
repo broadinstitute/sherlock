a403a747cc887467eeb11e3d0fd5790545cb230e
Jack Warren <jack@jackwarren.info>
2024-06-05 15:26:58 -0400

    google workspace engine

---
+++ sherlock/internal/role_propagation/propagation_engines/google_workspace_group.go
@@ -0,0 +1,127 @@
+package propagation_engines
+
+import (
+	"context"
+	"fmt"
+	"github.com/broadinstitute/sherlock/go-shared/pkg/utils"
+	"github.com/broadinstitute/sherlock/sherlock/internal/models"
+	"github.com/broadinstitute/sherlock/sherlock/internal/role_propagation/intermediary_user"
+	"github.com/knadh/koanf"
+	admin "google.golang.org/api/admin/directory/v1"
+	"google.golang.org/api/option"
+	"strings"
+)
+
+type GoogleWorkspaceGroupIdentifier struct {
+	Email string `koanf:"email"`
+}
+
+func (f GoogleWorkspaceGroupIdentifier) EqualTo(other intermediary_user.Identifier) bool {
+	switch other := other.(type) {
+	case GoogleWorkspaceGroupIdentifier:
+		return f.Email == other.Email
+	default:
+		return false
+	}
+}
+
+type GoogleWorkspaceGroupFields struct{}
+
+func (f GoogleWorkspaceGroupFields) EqualTo(other intermediary_user.Fields) bool {
+	switch other.(type) {
+	case GoogleWorkspaceGroupFields:
+		return true
+	default:
+		return false
+	}
+}
+
+type GoogleWorkspaceGroupEngine struct {
+	workspaceDomain           string
+	userEmailDomainsToReplace []string
+	adminService              *admin.Service
+}
+
+func (f *GoogleWorkspaceGroupEngine) Init(ctx context.Context, k *koanf.Koanf) error {
+	f.workspaceDomain = k.String("workspaceDomain")
+	f.userEmailDomainsToReplace = k.Strings("userEmailDomainsToReplace")
+	var err error
+	f.adminService, err = admin.NewService(ctx, option.WithScopes(admin.AdminDirectoryUserScope, admin.AdminDirectoryGroupMemberScope))
+	return err
+}
+
+func (f *GoogleWorkspaceGroupEngine) LoadCurrentState(ctx context.Context, grant string) ([]intermediary_user.IntermediaryUser[GoogleWorkspaceGroupIdentifier, GoogleWorkspaceGroupFields], error) {
+	currentState := make([]intermediary_user.IntermediaryUser[GoogleWorkspaceGroupIdentifier, GoogleWorkspaceGroupFields], 0)
+	err := f.adminService.Members.List(grant).Pages(ctx, func(members *admin.Members) error {
+		for _, member := range members.Members {
+			currentState = append(currentState, intermediary_user.IntermediaryUser[GoogleWorkspaceGroupIdentifier, GoogleWorkspaceGroupFields]{
+				Identifier: GoogleWorkspaceGroupIdentifier{Email: member.Email},
+				Fields:     GoogleWorkspaceGroupFields{},
+			})
+		}
+		return nil
+	})
+	return currentState, err
+}
+
+func (f *GoogleWorkspaceGroupEngine) GenerateDesiredState(ctx context.Context, roleAssignments map[uint]models.RoleAssignment) (map[uint]intermediary_user.IntermediaryUser[GoogleWorkspaceGroupIdentifier, GoogleWorkspaceGroupFields], error) {
+	desiredState := make(map[uint]intermediary_user.IntermediaryUser[GoogleWorkspaceGroupIdentifier, GoogleWorkspaceGroupFields])
+	for id, roleAssignment := range roleAssignments {
+		if !roleAssignment.IsActive() {
+			// There's no concept of a suspended group member, so we just exclude any non-active users
+			continue
+		}
+
+		email := utils.SubstituteEmailDomain(roleAssignment.User.Email, f.userEmailDomainsToReplace, f.workspaceDomain)
+		if !strings.HasSuffix(email, f.workspaceDomain) {
+			// We can short-circuit here, we know that the user is not in the workspace domain so we won't bother looking
+			continue
+		}
+
+		err := f.adminService.Users.List().
+			Domain(f.workspaceDomain).
+			Query("email="+email).
+			Fields("users(primaryEmail)").
+			MaxResults(1).
+			Pages(ctx, func(workspaceUsers *admin.Users) error {
+				for _, workspaceUser := range workspaceUsers.Users {
+					if workspaceUser.PrimaryEmail == email {
+						desiredState[id] = intermediary_user.IntermediaryUser[GoogleWorkspaceGroupIdentifier, GoogleWorkspaceGroupFields]{
+							Identifier: GoogleWorkspaceGroupIdentifier{Email: email},
+							Fields:     GoogleWorkspaceGroupFields{},
+						}
+					}
+				}
+				return nil
+			})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return desiredState, nil
+}
+
+func (f *GoogleWorkspaceGroupEngine) Add(ctx context.Context, grant string, identifier GoogleWorkspaceGroupIdentifier, _ GoogleWorkspaceGroupFields) (string, error) {
+	response, err := f.adminService.Members.Insert(grant, &admin.Member{
+		Role:  "MEMBER",
+		Email: identifier.Email,
+	}).Context(ctx).Do()
+	if err != nil {
+		return "", fmt.Errorf("failed to add %s to %s: %w", identifier.Email, grant, err)
+	} else {
+		return fmt.Sprintf("added %s to %s", response.Email, grant), nil
+	}
+}
+
+func (f *GoogleWorkspaceGroupEngine) Update(_ context.Context, _ string, _ GoogleWorkspaceGroupIdentifier, _ GoogleWorkspaceGroupFields, _ GoogleWorkspaceGroupFields) (string, error) {
+	return "", fmt.Errorf("%T.Update not implemented; %T.EqualTo should always equal true", f, GoogleWorkspaceGroupFields{})
+}
+
+func (f *GoogleWorkspaceGroupEngine) Remove(ctx context.Context, grant string, identifier GoogleWorkspaceGroupIdentifier) (string, error) {
+	err := f.adminService.Members.Delete(grant, identifier.Email).Context(ctx).Do()
+	if err != nil {
+		return "", fmt.Errorf("failed to remove %s from %s: %w", identifier.Email, grant, err)
+	} else {
+		return fmt.Sprintf("removed %s from %s", identifier.Email, grant), nil
+	}
+}
---
+++ sherlock/internal/role_propagation/propagation_engines/google_workspace_group_test.go
@@ -0,0 +1,153 @@
+package propagation_engines
+
+import (
+	"context"
+	"github.com/broadinstitute/sherlock/go-shared/pkg/utils"
+	"github.com/broadinstitute/sherlock/sherlock/internal/models"
+	"github.com/broadinstitute/sherlock/sherlock/internal/role_propagation/intermediary_user"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestGoogleWorkspaceGroupIdentifier_EqualTo(t *testing.T) {
+	type fields struct {
+		Email string
+	}
+	type args struct {
+		other intermediary_user.Identifier
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "equal",
+			fields: fields{
+				Email: "foo",
+			},
+			args: args{
+				other: GoogleWorkspaceGroupIdentifier{
+					Email: "foo",
+				},
+			},
+			want: true,
+		},
+		{
+			name: "not equal",
+			fields: fields{
+				Email: "foo",
+			},
+			args: args{
+				other: GoogleWorkspaceGroupIdentifier{
+					Email: "bar",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "different type",
+			fields: fields{
+				Email: "foo",
+			},
+			args: args{
+				other: AzureGroupIdentifier{
+					ID: "foo",
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := GoogleWorkspaceGroupIdentifier{
+				Email: tt.fields.Email,
+			}
+			assert.Equalf(t, tt.want, a.EqualTo(tt.args.other), "EqualTo(%v)", tt.args.other)
+		})
+	}
+}
+
+func TestGoogleWorkspaceGroupFields_EqualTo(t *testing.T) {
+	type args struct {
+		other intermediary_user.Fields
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "same type",
+			args: args{
+				other: GoogleWorkspaceGroupFields{},
+			},
+			want: true,
+		},
+		{
+			name: "different type",
+			args: args{
+				other: AzureGroupFields{},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := GoogleWorkspaceGroupFields{}
+			assert.Equalf(t, tt.want, f.EqualTo(tt.args.other), "EqualTo(%v)", tt.args.other)
+		})
+	}
+}
+
+// We can't easily test the actual cloud logic, but we can test that we short circuit correctly for
+// non-active role assignments.
+func TestGoogleWorkspaceGroupEngine_GenerateDesiredState_isActiveShortCircuit(t *testing.T) {
+	engine := &GoogleWorkspaceGroupEngine{}
+	desiredState, err := engine.GenerateDesiredState(context.Background(), map[uint]models.RoleAssignment{
+		1: {
+			RoleAssignmentFields: models.RoleAssignmentFields{
+				Suspended: utils.PointerTo(true),
+			},
+		},
+		2: {
+			RoleAssignmentFields: models.RoleAssignmentFields{
+				Suspended: utils.PointerTo(false),
+				ExpiresAt: utils.PointerTo(time.Now().Add(-time.Hour)),
+			},
+		},
+	})
+	assert.NoError(t, err)
+	assert.Empty(t, desiredState)
+}
+
+// We can't easily test the actual cloud logic, but we can test that we short circuit correctly for
+// emails that aren't in the target domain.
+//
+// See also utils.SubstituteEmailDomain
+func TestGoogleWorkspaceGroupEngine_GenerateDesiredState_emailShortCircuit(t *testing.T) {
+	engine := &GoogleWorkspaceGroupEngine{
+		workspaceDomain:           "example.com",
+		userEmailDomainsToReplace: []string{"example.org"},
+	}
+	desiredState, err := engine.GenerateDesiredState(context.Background(), map[uint]models.RoleAssignment{
+		1: {
+			User: &models.User{
+				Email: "user@example.net",
+			},
+			RoleAssignmentFields: models.RoleAssignmentFields{
+				Suspended: utils.PointerTo(false),
+			},
+		},
+	})
+	assert.NoError(t, err)
+	assert.Empty(t, desiredState)
+}
+
+func TestGoogleWorkspaceGroupEngine_Update_errors(t *testing.T) {
+	engine := &GoogleWorkspaceGroupEngine{}
+	_, err := engine.Update(context.Background(), "", GoogleWorkspaceGroupIdentifier{}, GoogleWorkspaceGroupFields{}, GoogleWorkspaceGroupFields{})
+	assert.Error(t, err)
+}
