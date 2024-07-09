3d06b8a5e8065e15955cda122b16c1d98d7c1113
Jack Warren <jack@jackwarren.info>
2024-07-09 16:37:32 -0400

    account linking notif

--- sherlock/internal/api/sherlock/users_v3_upsert_test.go
+++ sherlock/internal/api/sherlock/users_v3_upsert_test.go
@@ -353,6 +353,7 @@ func (s *handlerSuite) Test_processUserEdits() {
 		githubMockConfig  func(c *github.MockClient)
 		wantResultingUser *models.User
 		wantHasUpdates    bool
+		wantShouldNotify  bool
 	}{
 		{
 			name: "can do nothing",
@@ -365,15 +366,17 @@ func (s *handlerSuite) Test_processUserEdits() {
 			githubMockConfig:  func(c *github.MockClient) {},
 			wantResultingUser: &models.User{Email: s.TestData.User_Suitable().Email},
 			wantHasUpdates:    false,
+			wantShouldNotify:  false,
 		},
 	}
 	for _, tt := range tests {
 		s.Run(tt.name, func() {
 			slack.UseMockedClient(s.T(), tt.slackMockConfig, func() {
 				github.UseMockedClient(s.T(), tt.githubMockConfig, func() {
-					gotResultingUser, gotHasUpdates := processUserEdits(tt.args.callingUser, tt.args.directEdits, tt.args.userGithubToken)
+					gotResultingUser, gotHasUpdates, gotShouldNotify := processUserEdits(tt.args.callingUser, tt.args.directEdits, tt.args.userGithubToken)
 					assert.Equalf(s.T(), tt.wantResultingUser, gotResultingUser, "processUserEdits(%v, %v, %v)", tt.args.callingUser, tt.args.directEdits, tt.args.userGithubToken)
 					assert.Equalf(s.T(), tt.wantHasUpdates, gotHasUpdates, "processUserEdits(%v, %v, %v)", tt.args.callingUser, tt.args.directEdits, tt.args.userGithubToken)
+					assert.Equalf(s.T(), tt.wantShouldNotify, gotShouldNotify, "processUserEdits(%v, %v, %v)", tt.args.callingUser, tt.args.directEdits, tt.args.userGithubToken)
 				})
 			})
 		})
