c6f80703809a1ce2504c113c678fa4c34fba5141
Jack Warren <jack@jackwarren.info>
2024-07-17 14:49:12 -0400

    oops

--- sherlock/db/migrations/000094_add_enable_janitor.up.sql
+++ sherlock/db/migrations/000094_add_enable_janitor.up.sql
@@ -2,7 +2,7 @@ alter table environments
     add column if not exists enable_janitor boolean;
 
 update environments
-    set enable_janitor = (case when lifecycle = 'static' then true else false end)
+    set enable_janitor = (case when lifecycle = 'static' then false else true end)
     where enable_janitor is null;
 
 alter table environments
