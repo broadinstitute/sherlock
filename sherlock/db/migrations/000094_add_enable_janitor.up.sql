e5a4127a48d6c946d07d024f02046c66c22bc3bd
Jack Warren <jack@jackwarren.info>
2024-07-17 15:37:04 -0400

    [DDO-3789] Enable Janitor Toggle (#609)

---
+++ sherlock/db/migrations/000094_add_enable_janitor.up.sql
@@ -0,0 +1,12 @@
+alter table environments
+    add column if not exists enable_janitor boolean;
+
+update environments
+    set enable_janitor = (case when lifecycle = 'static' then false else true end)
+    where enable_janitor is null;
+
+alter table environments
+    alter column enable_janitor set default false;
+
+alter table environments
+    alter column enable_janitor set not null;
