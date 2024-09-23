8e33edc0e38772782948271ecb24c33a8e7b45d3
broadbot <broadbot@broadinstitute.org>
2024-09-23 03:00:38 +0000

    [sherlock-build] generated from 9f965869a6fe97b7a67b5cfba40696bcc940b407

--- sherlock-python-client/setup.py
+++ sherlock-python-client/setup.py
@@ -22,10 +22,10 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "sherlock-python-client"
-VERSION = "v1.5.51"
+VERSION = "v1.5.52"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
-    "urllib3 >= 1.25.3, < 2.1.0",
+    "urllib3 >= 1.25.3, < 3.0.0",
     "python-dateutil",
     "pydantic >= 2",
     "typing-extensions >= 4.7.1",
