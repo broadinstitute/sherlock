0a3bfb905d40ee6db20b6f76dbf2e5a3f833f6b9
broadbot <broadbot@broadinstitute.org>
2025-04-24 16:05:48 +0000

    [sherlock-build] generated from 8aa0e682adb92a69574175eecd04ce4ff3a76b43

--- sherlock-python-client/setup.py
+++ sherlock-python-client/setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "sherlock-python-client"
-VERSION = "v1.6.25"
+VERSION = "v1.6.26"
 PYTHON_REQUIRES = ">= 3.9"
 REQUIRES = [
     "urllib3 >= 2.1.0, < 3.0.0",
--- sherlock-python-client/sherlock_python_client/__init__.py
+++ sherlock-python-client/sherlock_python_client/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "v1.6.25"
+__version__ = "v1.6.26"
 
 # import apis into sdk package
 from sherlock_python_client.api.app_versions_api import AppVersionsApi
