fb8c7c6b9de5667f5af75df17dc1d8873a6eab41
broadbot <broadbot@broadinstitute.org>
2025-05-12 02:47:13 +0000

    [sherlock-build] generated from c9fafd911bbd403a5692e2564640debeab6382b8

--- sherlock-python-client/sherlock_python_client/api_client.py
+++ sherlock-python-client/sherlock_python_client/api_client.py
@@ -91,7 +91,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/v1.6.32/python'
+        self.user_agent = 'OpenAPI-Generator/v1.6.33/python'
         self.client_side_validation = configuration.client_side_validation
 
     def __enter__(self):
