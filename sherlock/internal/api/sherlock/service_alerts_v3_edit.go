3c53e63f1211400707ddb1dd43d82c432b9fd6bd
John Scira <jscira@broadinstitute.org>
2025-06-06 10:35:07 -0400

    adding edit endpoint

---
+++ sherlock/internal/api/sherlock/service_alerts_v3_edit.go
@@ -0,0 +1,46 @@
+package sherlock
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/broadinstitute/sherlock/sherlock/internal/errors"
+	"github.com/broadinstitute/sherlock/sherlock/internal/middleware/authentication"
+	"github.com/broadinstitute/sherlock/sherlock/internal/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm/clause"
+)
+
+func serviceAlertV3Edit(ctx *gin.Context) {
+	db, err := authentication.MustUseDB(ctx)
+	if err != nil {
+		return
+	}
+	query, err := roleModelFromSelector(canonicalizeSelector(ctx.Param("selector")))
+	if err != nil {
+		errors.AbortRequest(ctx, err)
+		return
+	}
+
+	var body ServiceAlertV3
+	if err = ctx.ShouldBindJSON(&body); err != nil {
+		errors.AbortRequest(ctx, fmt.Errorf("(%s) request validation error: %w", errors.BadRequest, err))
+		return
+	}
+
+	edits := body.toModel(db)
+
+	var toEdit models.ServiceAlert
+	if err = db.Where(&query).First(&toEdit).Error; err != nil {
+		errors.AbortRequest(ctx, err)
+		return
+	}
+
+	if err = db.Model(&toEdit).Omit(clause.Associations).Updates(&edits).Error; err != nil {
+		errors.AbortRequest(ctx, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, ServiceAlertFromModel(toEdit))
+
+}
