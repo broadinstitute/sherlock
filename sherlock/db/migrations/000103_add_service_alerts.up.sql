5fd5beb222ce759c36801c60a1a8b33dd9910693
John Scira <jscira@broadinstitute.org>
2025-06-09 15:59:51 -0400

    updating db migrations

--- sherlock/db/migrations/000103_add_service_alerts.up.sql
+++ sherlock/db/migrations/000103_add_service_alerts.up.sql
@@ -6,10 +6,15 @@ create table if not exists service_alerts
     updated_at                     timestamp with time zone,
     deleted_at                     timestamp with time zone,
 
+    on_environment_id   bigint
+        constraint fk_service_alerts_on_environment
+            references environments,
+
     title                           text not null,
-	message                           text not null,
+	alert_message                           text not null,
 	link                           text,
     severity   text
+
 );
 
 create index if not exists svc_alerts_deleted_at
