89b38806417dcbe776dd9173897d9e38403f258d
John Scira <jscira@broadinstitute.org>
2025-07-01 13:20:46 -0400

    formatting updates for clarity

--- sherlock/internal/api/sherlock/service_alerts_procedures_v3_sync_test.go
+++ sherlock/internal/api/sherlock/service_alerts_procedures_v3_sync_test.go
@@ -12,80 +12,82 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-func (s *handlerSuite) TestSyncServiceAlerts_Success() {
-	s.SetNonSuitableTestUserForDB()
+func (test_handler *handlerSuite) TestSyncServiceAlerts_Success() {
+	test_handler.SetNonSuitableTestUserForDB()
 	env := s.TestData.Environment_Dev()
 	alert1 := s.TestData.ServiceAlert_1()
 	alert1.OnEnvironmentID = &env.ID
-	s.NoError(s.DB.Save(&alert1).Error)
+	test_handler.NoError(test_handler.DB.Save(&alert1).Error)
 
-	google_bucket.UseMockedClient(s.T(), func(c *google_bucket_mocks.MockgcsClient) {
+	google_bucket.UseMockedClient(test_handler.T(), func(c *google_bucket_mocks.MockgcsClient) {
 		c.EXPECT().WriteBlob(mock.Anything, *env.ServiceBannerBucket, "alerts.json", mock.Anything).Return(nil).Once()
 	}, func() {
 		var got []ServiceAlertV3
-		code := s.HandleRequest(
-			s.NewRequest("POST", "/api/service-alerts/procedures/v3/sync", ServiceAlertV3SyncRequest{
+		code := test_handler.HandleRequest(
+			test_handler.NewRequest("POST", "/api/service-alerts/procedures/v3/sync", ServiceAlertV3SyncRequest{
 				OnEnvironment: env.Name,
 			}),
 			&got)
-		s.Equal(http.StatusOK, code)
-		s.Len(got, 1)
-		s.Equal(*alert1.Title, *got[0].Title)
+		test_handler.Equal(http.StatusOK, code)
+		test_handler.Len(got, 1)
+		test_handler.Equal(*alert1.Title, *got[0].Title)
 	})
 }
 
-func (s *handlerSuite) TestSyncServiceAlerts_WriteBlobFails() {
-	s.SetNonSuitableTestUserForDB()
+// test to write blob
+func (test_handler *handlerSuite) TestSyncServiceAlerts_WriteBlobFails() {
+	test_handler.SetNonSuitableTestUserForDB()
 	env := s.TestData.Environment_Dev()
 	alert1 := s.TestData.ServiceAlert_1()
 	alert1.OnEnvironmentID = &env.ID
-	s.NoError(s.DB.Save(&alert1).Error)
+	test_handler.NoError(test_handler.DB.Save(&alert1).Error)
 
-	google_bucket.UseMockedClient(s.T(), func(c *google_bucket_mocks.MockgcsClient) {
+	google_bucket.UseMockedClient(test_handler.T(), func(c *google_bucket_mocks.MockgcsClient) {
 		c.EXPECT().WriteBlob(mock.Anything, *env.ServiceBannerBucket, "alerts.json", mock.Anything).Return(fmt.Errorf("some GCS error")).Once()
 	}, func() {
 		var got errors.ErrorResponse
 		code := s.HandleRequest(
-			s.NewRequest("POST", "/api/service-alerts/procedures/v3/sync", ServiceAlertV3SyncRequest{
+			test_handler.NewRequest("POST", "/api/service-alerts/procedures/v3/sync", ServiceAlertV3SyncRequest{
 				OnEnvironment: env.Name,
 			}),
 			&got)
-		s.Equal(http.StatusInternalServerError, code)
-		s.Contains(got.Message, "error writing updated alerts.json file")
+		test_handler.Equal(http.StatusInternalServerError, code)
+		test_handler.Contains(got.Message, "error writing updated alerts.json file")
 	})
 }
 
-func (s *handlerSuite) TestSyncServiceAlerts_NoAlerts() {
-	s.SetNonSuitableTestUserForDB()
-	env := s.TestData.Environment_Dev()
+func (test_handler *handlerSuite) TestSyncServiceAlerts_NoAlerts() {
+	test_handler.SetNonSuitableTestUserForDB()
+	env := test_handler.TestData.Environment_Dev()
 
 	// Ensure there are no alerts for this environment by clearing any existing ones
-	s.NoError(s.DB.Where("on_environment_id = ?", env.ID).Delete(&models.ServiceAlert{}).Error)
+	test_handler.NoError(test_handler.DB.Where("on_environment_id = ?", env.ID).Delete(&models.ServiceAlert{}).Error)
 
 	var got errors.ErrorResponse
-	code := s.HandleRequest(
-		s.NewRequest("POST", "/api/service-alerts/procedures/v3/sync", ServiceAlertV3SyncRequest{
+	code := test_handler.HandleRequest(
+		test_handler.NewRequest("POST", "/api/service-alerts/procedures/v3/sync", ServiceAlertV3SyncRequest{
 			OnEnvironment: env.Name,
 		}),
 		&got)
-	s.Equal(http.StatusBadRequest, code)
-	s.Contains(got.Message, "No Alerts found for this environment")
+	test_handler.Equal(http.StatusBadRequest, code)
+	test_handler.Contains(got.Message, "No Alerts found for this environment")
 }
 
-func (s *handlerSuite) TestCreateServiceAlertJsonData() {
-	alert1 := s.TestData.ServiceAlert_1()
-	alert2 := s.TestData.ServiceAlert_2()
+// Ensure service alert model data can be transformed into json.
+func (test_handler *handlerSuite) TestCreateServiceAlertJsonData() {
+	alert1 := test_handler.TestData.ServiceAlert_1()
+	alert2 := test_handler.TestData.ServiceAlert_2()
 	alerts := []models.ServiceAlert{alert1, alert2}
 	jsonData, err := createServiceAlertJsonData(alerts)
-	s.NoError(err)
+	test_handler.NoError(err)
 
 	var got []ServiceAlertJsonData
 	err = json.Unmarshal(jsonData, &got)
-	s.NoError(err)
+	test_handler.NoError(err)
 
-	s.Len(got, 2)
-	s.Equal(*alert1.Title, got[0].Title)
-	s.Equal(alert1.Uuid.String(), got[0].IncidentID)
-	s.Equal(*alert2.Title, got[1].Title)
-	s.Equal(alert2.Uuid.String(), got[1].IncidentID)
+	test_handler.Len(got, 2)
+	test_handler.Equal(*alert1.Title, got[0].Title)
+	test_handler.Equal(alert1.Uuid.String(), got[0].IncidentID)
+	test_handler.Equal(*alert2.Title, got[1].Title)
+	test_handler.Equal(alert2.Uuid.String(), got[1].IncidentID)
 }
