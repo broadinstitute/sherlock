b91cc52fea369b261ad89320c82a7e4cf2a57d37
John Scira <jscira@broadinstitute.org>
2025-07-18 11:35:11 -0400

    adding suitability test

--- sherlock/internal/api/sherlock/service_alerts_v3_create_test.go
+++ sherlock/internal/api/sherlock/service_alerts_v3_create_test.go
@@ -20,7 +20,7 @@ func (s *handlerSuite) TestServiceAlertV3Create_badBody() {
 	s.Contains(got.Message, "severity")
 }
 
-func (s *handlerSuite) TestServiceAlertV3Create() {
+func (s *handlerSuite) TestServiceAlertV3CreateSuitable() {
 	var got ServiceAlertV3
 	code := s.HandleRequest(
 		s.NewRequest("POST", "/api/service-alerts/v3", ServiceAlertV3Create{
@@ -37,4 +37,21 @@ func (s *handlerSuite) TestServiceAlertV3Create() {
 	if s.NotNil(got.Title) {
 		s.Equal("Service Alert Title", *got.Title)
 	}
+}
+
+func (s *handlerSuite) TestServiceAlertV3CreateNonSuitableUser() {
+	var got errors.ErrorResponse
+	code := s.HandleRequest(
+		s.UseNonSuitableUserFor(s.NewRequest("POST", "/api/service-alerts/v3", ServiceAlertV3Create{
+			OnEnvironment: utils.PointerTo(s.TestData.Environment_Prod().Name),
+			ServiceAlertV3EditableFields: ServiceAlertV3EditableFields{
+				AlertMessage: utils.PointerTo("Alert message here"),
+				Title:        utils.PointerTo("Service Alert Title"),
+				Link:         utils.PointerTo("Link here"),
+				Severity:     utils.PointerTo("blocker"),
+			},
+		})),
+		&got)
+	s.Equal(http.StatusForbidden, code)
+	s.Equal(errors.Forbidden, got.Type)
 }
